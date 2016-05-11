c3a1dc40f12967d498dfb34f3f8ce2972d74430d
Cody Maloney <cmaloney@theoreticalchaos.com>
2016-05-10 22:31:49 -0700

    Move AWS and Azure ip-detect scripts to be loaded out of the python package rather than assumed to be in the filesystem

--- gen/azure/calc.py
+++ gen/azure/calc.py
@@ -1,7 +1,11 @@
+import pkg_resources
+
+import yaml
+
 entry = {
     'must': {
         'resolvers': '["168.63.129.16"]',
-        'ip_detect_filename': 'gen/ip-detect/azure.sh',
+        'ip_detect_contents': yaml.dump(pkg_resources.resource_string('gen', 'ip-detect/aws.sh').decode()),
         'master_discovery': 'static',
         'exhibitor_storage_backend': 'azure',
         'master_cloud_config': '{{ master_cloud_config }}',
