b0cc15de6322f45a5342eb748a39384a7e9daa1c
Cody Maloney <cody@mesosphere.io>
2016-05-24 16:19:56 -0700

    Add a build_local script to help doing dev builds

---
+++ build_local.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+# Simple helper script to do a full local  build
+
+set -x
+set -o errexit -o pipefail
+
+# Fail quickly if docker isn't working / up
+docker ps
+
+# Cleanup from previous build
+rm -rf build/venv
+
+# Force Python stdout/err to be unbuffered.
+export PYTHONUNBUFFERED="notemtpy"
+
+# Write a DC/OS Release tool configuration file which specifies where the build
+# should be published to. This default config makes the release tool push the
+# release to a folder in the current user's home directory.
+if [ ! -f dcos-release.config.yaml ]; then
+cat <<EOF > dcos-release.config.yaml
+storage:
+  local:
+    kind: local_path
+    path: $HOME/dcos-artifacts
+options:
+  preferred: local
+  cloudformation_s3_url: https://change_me_to_use_the_aws_templates_and_webpages/
+EOF
+fi
+
+# Create a python virtual environment to install the DC/OS tools to
+pyvenv build/env
+. build/env/bin/activate
+
+# Install the DC/OS tools
+./prep_local
+
+# Build a release of DC/OS
+release create `whoami` local_build
