fbc2642b0361d48579c6556817fab02e9a7cfda8
Cody Maloney <cody@mesosphere.io>
2016-05-26 14:28:18 -0700

    Fix Azure to use it's ip-detect script rather than AWS'

--- gen/azure/calc.py
+++ gen/azure/calc.py
@@ -5,7 +5,7 @@ import yaml
 entry = {
     'must': {
         'resolvers': '["168.63.129.16"]',
-        'ip_detect_contents': yaml.dump(pkg_resources.resource_string('gen', 'ip-detect/aws.sh').decode()),
+        'ip_detect_contents': yaml.dump(pkg_resources.resource_string('gen', 'ip-detect/azure.sh').decode()),
         'master_discovery': 'static',
         'exhibitor_storage_backend': 'azure',
         'master_cloud_config': '{{ master_cloud_config }}',
