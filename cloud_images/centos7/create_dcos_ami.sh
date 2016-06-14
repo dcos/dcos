95cb834bfe3ae0c76569b14cf5fc1f9b8f804b01
Jeremy Lingmann <jeremy@mesosphere.io>
2016-06-14 07:01:35 -0700

    Scripts to create DC/OS compatible cloud images

---
+++ cloud_images/centos7/create_dcos_ami.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -o errexit -o nounset -o pipefail
+
+# AWS profile with appropriate credentials for Packer to create the AMI
+export AWS_PROFILE=${AWS_PROFILE:-"development"}
+
+# Base CentOS 7 AMI and region
+export SOURCE_AMI=${SOURCE_AMI:-"ami-c5af54a5"}
+export SOURCE_AMI_REGION=${SOURCE_AMI_REGION:-"us-west-2"}
+
+# Comma separated string of AWS regions to copy the resulting DC/OS AMI to
+export DEPLOY_REGIONS=${DEPLOY_REGIONS:-"us-west-2"}
+
+# Useful options include -debug and -machine-readable
+PACKER_BUILD_OPTIONS=${PACKER_BUILD_OPTIONS:-""}
+
+packer build $PACKER_BUILD_OPTIONS packer.json
