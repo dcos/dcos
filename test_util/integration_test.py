9c09344a103780627c6d1f59aeb2af1a3c5667d0
malnick <malnick@gmail..com>
2016-06-28 09:19:00 -0700

    fix signal test

--- test_util/integration_test.py
+++ test_util/integration_test.py
@@ -1428,8 +1428,7 @@ sleep 3600
         'spartan-watchdog-service',
         'spartan-watchdog-timer']
     slave_units = [
-        'health-unit-dcos-3dt-socket-total',
-        'health-unit-dcos-3dt-socket-unhealthy',
+        '3dt-socket',
         'mesos-slave-service',
         'vol-discovery-priv-agent-service']
     public_slave_units = [
