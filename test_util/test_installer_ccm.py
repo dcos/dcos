ae71a735c58d75eec83eb6aac599e73de3b12c5f
malnick <malnick@gmail..com>
2016-07-06 08:24:37 -0700

    newlines

--- test_util/test_installer_ccm.py
+++ test_util/test_installer_ccm.py
@@ -307,9 +307,7 @@ def main():
         else:
             # Test bad config cases
             log.info("Configuring with bad SSH key (pub key test)")
-            pub_key = """
-ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDDlq/HSU9B5HyCTdSh6opW6NpXT3uxBYrDNnNj4YllVOyp/HCucHorSn40vwtAQNDWWZD3jUFXdB7RN3qVqrfYXrfsNp12Pb5pKsiDZcQbe+rRMOaaBXQ7wylDWj/rp0SIH7AIYnZ8PzEb4PXqCM0MaVMZukUFb8frvv8B0I1YwMJiK0qocVAuacWhWmXJ29zktpqfuHE5RArCycVR6QU+6/EvyDfjL/HM+YYz3DMDvOH9PxG6uOuiYSsXBo02H2y4GhI/HhaUPJMmUHweIbFZcn6RMEF0nNIULnlDNPTsDTtuDY4p5Li/pLDtU71MIlp5ubbgCBA9UolUCoKSkDFN root@bohr\n
-"""  # noqa
+            pub_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDDlq/HSU9B5HyCTdSh6opW6NpXT3uxBYrDNnNj4YllVOyp/HCucHorSn40vwtAQNDWWZD3jUFXdB7RN3qVqrfYXrfsNp12Pb5pKsiDZcQbe+rRMOaaBXQ7wylDWj/rp0SIH7AIYnZ8PzEb4PXqCM0MaVMZukUFb8frvv8B0I1YwMJiK0qocVAuacWhWmXJ29zktpqfuHE5RArCycVR6QU+6/EvyDfjL/HM+YYz3DMDvOH9PxG6uOuiYSsXBo02H2y4GhI/HhaUPJMmUHweIbFZcn6RMEF0nNIULnlDNPTsDTtuDY4p5Li/pLDtU71MIlp5ubbgCBA9UolUCoKSkDFN root@bohr"  # noqa
             installer.genconf(
                 expect_errors=True,
                 zk_host=zk_host,
