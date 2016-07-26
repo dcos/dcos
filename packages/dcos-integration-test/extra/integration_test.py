f305849aca1eb9ca440da45e0b0a58d2e656e9de
Sargun Dhillon <sargun@sargun.me>
2016-07-25 17:52:33 -0700

    Cleanup original minuteman test

--- packages/dcos-integration-test/extra/integration_test.py
+++ packages/dcos-integration-test/extra/integration_test.py
@@ -386,7 +386,7 @@ class Cluster:
                 'type': 'DOCKER',
                 'docker': {
                     'image': '{}/test_server'.format(self.registry),
-                    'forcePullImage': True,
+                    'forcePullImage': True
                 },
             },
             'cmd': '/opt/test_server.py 9080',
@@ -412,21 +412,25 @@ class Cluster:
 
         if docker_network_bridge:
             base_app['container']['docker']['portMappings'] = [{
-                'containerPort':  9080,
-                'hostPort': 0,
-                'servicePort': 0,
-                'protocol': 'tcp',
-            }]
+                                                                    'hostPort': 0,
+                                                                    'containerPort': 9080,
+                                                                    'protocol': 'tcp',
+                                                                    'name': 'test',
+                                                                    'labels': {}
+                                                                }]
             if ip_per_container:
                 base_app['container']['docker']['network'] = 'USER'
                 base_app['ipAddress'] = {'networkName': 'dcos'}
             else:
                 base_app['container']['docker']['network'] = 'BRIDGE'
-                base_app['ports'] = []
         else:
+            base_app['portDefinitions'] = [{
+                                                "protocol": "tcp",
+                                                "port": 0,
+                                                "name": "test"
+                                            }]
             base_app['cmd'] = '/opt/test_server.py $PORT0'
             base_app['container']['docker']['network'] = 'HOST'
-            base_app['ports'] = [0]
 
         return base_app, test_uuid
 
@@ -1037,75 +1041,29 @@ def test_octarine_srv(cluster, timeout=30):
 
 # By default telemetry-net sends the metrics about once a minute
 # Therefore, we wait up till 2 minutes and a bit before we give up
-def test_if_minuteman_routes_to_vip(cluster, timeout=125):
+def test_if_minuteman_routes_to_vip(registry_cluster, timeout=125):
     """Test if we are able to connect to a task with a vip using minuteman.
     """
-    # Launch the app and proxy
-    test_uuid = uuid.uuid4().hex
 
-    app_definition = {
-        'id': "/integration-test-app-with-minuteman-vip-%s" % test_uuid,
-        'cpus': 0.1,
-        'mem': 128,
-        'cmd': 'touch imok && /opt/mesosphere/bin/python -mhttp.server ${PORT0}',
-        'portDefinitions': [
-            {
-                'port': 0,
-                'protocol': 'tcp',
-                'name': 'test',
-                'labels': {
-                    'VIP_0': '1.2.3.4:5000'
-                }
-            }
-        ],
-        'uris': [],
-        'instances': 1,
-        'healthChecks': [{
-            'protocol': 'HTTP',
-            'path': '/',
-            'portIndex': 0,
-            'gracePeriodSeconds': 5,
-            'intervalSeconds': 10,
-            'timeoutSeconds': 10,
-            'maxConsecutiveFailures': 3
-        }]
-    }
-
-    cluster.deploy_marathon_app(app_definition)
-
-    proxy_definition = {
-        'id': "/integration-test-proxy-to-minuteman-vip-%s" % test_uuid,
-        'cpus': 0.1,
-        'mem': 128,
-        'ports': [0],
-        'cmd': 'chmod 755 ncat && ./ncat -v --sh-exec "./ncat 1.2.3.4 5000" -l $PORT0 --keep-open',
-        'uris': ['https://s3.amazonaws.com/sargun-mesosphere/ncat'],
-        'instances': 1,
-        'healthChecks': [{
-            'protocol': 'COMMAND',
-            'command': {
-                'value': 'curl -f -X GET http://$HOST:$PORT0/'
-            },
-            'gracePeriodSeconds': 0,
-            'intervalSeconds': 5,
-            'timeoutSeconds': 20,
-            'maxConsecutiveFailures': 3,
-            'ignoreHttp1xx': False
-        }],
-    }
-
-    service_points = cluster.deploy_marathon_app(proxy_definition)
+    cluster = registry_cluster
+    origin_app, origin_uuid = cluster.get_base_testapp_definition()
+    origin_app['container']['docker']['portMappings'][0]['labels']['VIP_0'] = '1.2.3.4:5000'
+    cluster.deploy_marathon_app(origin_app)
+    
+    proxy_app, proxy_uuid = cluster.get_base_testapp_definition()
+    service_points = cluster.deploy_marathon_app(proxy_app)
 
     @retrying.retry(wait_fixed=2000,
                     stop_max_delay=timeout*1000,
                     retry_on_result=lambda ret: ret is False,
                     retry_on_exception=lambda x: False)
     def _ensure_routable():
-        r = requests.get('http://{}:{}'.format(service_points[0].host,
-                                               service_points[0].port))
-        assert(r.ok)
-        data = r.text
-        assert 'imok' in data
+        cmd = 'curl -s -f http://1.2.3.4:5000/ping'
+        proxy_uri = 'http://{}:{}/run_cmd'.format(
+          service_points[0].host, service_points[0].port)
+        r = requests.post(proxy_uri, data=cmd)
+        logging.info('Curl Response: %s', repr(r.json()))
+        assert(r.json()['status'] == 0)
 
     _ensure_routable()
 
