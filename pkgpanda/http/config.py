a86e4bb4097313aa94b033bcdae04a7256b48ba1
Senthil Kumaran <skumaran@mesosphere.com>
2016-08-08 11:50:05 -0700

    Use tempfile.tempdir in http.config instead of hardcoding to /tmp.

--- pkgpanda/http/config.py
+++ pkgpanda/http/config.py
@@ -1,3 +1,6 @@
+import os
+import tempfile
+
 from pkgpanda import constants
 
 
@@ -6,4 +9,4 @@ DCOS_CONFIG_DIR = constants.config_dir
 DCOS_REPO_DIR = constants.repository_base
 DCOS_ROOTED_SYSTEMD = False
 
-WORK_DIR = '/tmp/pkgpanda_api'
+WORK_DIR = os.path.join(tempfile.gettempdir(), 'pkgpanda_api')
