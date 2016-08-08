10d0fdbcf17894185b4f755dde0f2139efd8a274
Branden Rolston <branden@mesosphere.com>
2016-08-08 14:34:27 -0700

    pkgpanda.write_json() -> pkgpanda.util.write_json()
    `write_json()` is only available in the `pkgpanda` namespace by
    coincidence, because it happens to import it from `pkgpanda.util`.

--- ext/dcos-installer/dcos_installer/action_lib/configure.py
+++ ext/dcos-installer/dcos_installer/action_lib/configure.py
@@ -27,7 +27,7 @@ def do_configure(gen_config):
     # Get packages from artifacts
     fetch_packages(ARTIFACT_DIR, SERVE_DIR)
     # Write some package metadata
-    pkgpanda.write_json('/genconf/cluster_packages.json', gen_out.cluster_packages)
+    pkgpanda.util.write_json('/genconf/cluster_packages.json', gen_out.cluster_packages)
 
 
 def get_gen_extra_args():
