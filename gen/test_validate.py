2f3b7c3b3d583852b4c95f5b34c9660edcbeb474
Cody Maloney <cmaloney@theoreticalchaos.com>
2016-08-29 18:50:49 -0700

    Re-format before code changes

--- gen/test_validate.py
+++ gen/test_validate.py
@@ -10,37 +10,37 @@ def test_error_during_calc():
     logger = logging.getLogger()
     logger.setLevel(logging.DEBUG)
     assert gen.validate({
-            'ip_detect_filename': 'not-a-existing-file',
-            'provider': 'onprem',
-            'bootstrap_variant': ''
-        }) == {
-            'status': 'errors',
-            'errors': {
-                'ip_detect_contents': {'message': 'ip-detect script: not-a-existing-file must exist'}
-            },
-            'unset': {
-                'bootstrap_id',
-                'bootstrap_url',
-                'cluster_name',
-                'exhibitor_storage_backend',
-                'master_discovery'
-            }
+        'ip_detect_filename': 'not-a-existing-file',
+        'provider': 'onprem',
+        'bootstrap_variant': ''
+    }) == {
+        'status': 'errors',
+        'errors': {
+            'ip_detect_contents': {'message': 'ip-detect script: not-a-existing-file must exist'}
+        },
+        'unset': {
+            'bootstrap_id',
+            'bootstrap_url',
+            'cluster_name',
+            'exhibitor_storage_backend',
+            'master_discovery'
         }
+    }
 
 
 def test_error_during_validate():
     logger = logging.getLogger()
     logger.setLevel(logging.DEBUG)
     assert gen.validate({
-            'bootstrap_url': '',
-            'bootstrap_variant': ''
-        }) == {
-            'status': 'errors',
-            'errors': {
-                'bootstrap_url': {'message': 'Should be a url (http://example.com/bar or file:///path/to/local/cache)'}
-            },
-            'unset': {
-                'bootstrap_id',
-                'provider',
-            }
+        'bootstrap_url': '',
+        'bootstrap_variant': ''
+    }) == {
+        'status': 'errors',
+        'errors': {
+            'bootstrap_url': {'message': 'Should be a url (http://example.com/bar or file:///path/to/local/cache)'}
+        },
+        'unset': {
+            'bootstrap_id',
+            'provider',
         }
+    }
