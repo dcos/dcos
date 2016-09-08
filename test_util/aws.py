1fc1c6cdf7f537183a9b49adeb4249be050dfd8e
Michael Ellenburg <michael.ellenburg@mesosphere.io>
2016-09-08 10:29:18 -0700

    Replace CCM VPC provider with AWS VPC provider

--- test_util/aws.py
+++ test_util/aws.py
@@ -10,6 +10,16 @@ logger = logging.getLogger(__name__)
 
 Host = namedtuple('Host', ['private_ip', 'public_ip'])
 
+VPC_TEMPLATE_URL = 'https://s3.amazonaws.com/vpc-cluster-template/vpc-cluster-template.json'
+VPC_EBS_ONLY_TEMPLATE_URL = 'https://s3.amazonaws.com/vpc-cluster-template/vpc-ebs-only-cluster-template.json'
+
+
+def template_by_instance_type(instance_type):
+    if instance_type.split('.')[0] in ('c4', 't2', 'm4'):
+        return VPC_EBS_ONLY_TEMPLATE_URL
+    else:
+        return VPC_TEMPLATE_URL
+
 
 def instances_to_hosts(instances):
     return [Host(i['PrivateIpAddress'], i['PublicIpAddress'] if 'PublicIpAddress' in i else None) for i in instances]
@@ -115,3 +125,117 @@ class DcosCfSimple(CfStack):
     def get_private_agent_ips(self):
         instances = self.get_tag_instances('SlaveServerGroup')
         return instances_to_hosts(instances)
+
+
+class VpcCfStack(CfStack):
+    @classmethod
+    def create(cls, stack_name, instance_type, instance_os, instance_count,
+               admin_location, key_pair_name, boto_wrapper):
+        ami_code = OS_AMIS[instance_os][boto_wrapper.region]
+        template_url = template_by_instance_type(instance_type)
+        parameters = {
+                'KeyPair': key_pair_name,
+                'AllowAccessFrom': admin_location,
+                'ClusterSize': str(instance_count),
+                'InstanceType': str(instance_type),
+                'AmiCode': ami_code
+                }
+        stack = boto_wrapper.create_stack(stack_name, template_url, parameters)
+        return cls(stack.stack.stack_id, boto_wrapper)
+
+    def delete(self):
+        # boto stacks become unusable after deletion (e.g. status/info checks) if name-based
+        self.stack = self.boto_wrapper.resource('cloudformation').Stack(self.stack.stack_id)
+        self.stack.delete()
+
+    def get_vpc_host_ips(self):
+        reservations = self.boto_wrapper.client('ec2').describe_instances(Filters=[{
+                'Name': 'tag-value', 'Values': [self.stack.stack_name]}])['Reservations']
+        logging.debug('Reservations for {}: {}'.format(self.stack.stack_id, reservations))
+        instances = reservations[0]['Instances']
+        return instances_to_hosts(instances)
+
+
+OS_AMIS = {
+    'cent-os-6': {'ap-northeast-1': 'ami-82640282',
+                  'ap-southeast-1': 'ami-44617116',
+                  'ap-southeast-2': 'ami-7b81ca41',
+                  'eu-central-1': 'ami-2a868b37',
+                  'eu-west-1': 'ami-2b7f4c5c',
+                  'sa-east-1': 'ami-fd0f99e0',
+                  'us-east-1': 'ami-57cd8732',
+                  'us-west-1': 'ami-45844401',
+                  'us-west-2': 'ami-1255b321'},
+    'cent-os-7': {'ap-northeast-1': 'ami-965345f8',
+                  'ap-southeast-1': 'ami-332de750',
+                  'ap-southeast-2': 'ami-c80320ab',
+                  'eu-central-1': 'ami-1548ae7a',
+                  'eu-west-1': 'ami-2ea92f5d',
+                  'sa-east-1': 'ami-2921ad45',
+                  'us-east-1': 'ami-fa9b9390',
+                  'us-west-1': 'ami-12b3ce72',
+                  'us-west-2': 'ami-edf11b8d'},
+    'cent-os-7-dcos-prereqs': {'ap-northeast-1': 'ami-965345f8',
+                               'ap-southeast-1': 'ami-332de750',
+                               'ap-southeast-2': 'ami-c80320ab',
+                               'eu-central-1': 'ami-1548ae7a',
+                               'eu-west-1': 'ami-2ea92f5d',
+                               'sa-east-1': 'ami-2921ad45',
+                               'us-east-1': 'ami-fa9b9390',
+                               'us-west-1': 'ami-12b3ce72',
+                               'us-west-2': 'ami-edf11b8d'},
+    'coreos': {'ap-northeast-1': 'ami-84e0c7ea',
+               'ap-southeast-1': 'ami-84e0c7ea',
+               'ap-southeast-2': 'ami-f35b0590',
+               'eu-central-1': 'ami-fdd4c791',
+               'eu-west-1': 'ami-55d20b26',
+               'sa-east-1': 'ami-f35b0590',
+               'us-east-1': 'ami-37bdc15d',
+               'us-west-1': 'ami-27553a47',
+               'us-west-2': 'ami-00ebfc61'},
+    'debian-8': {'ap-northeast-1': 'ami-fe54f3fe',
+                 'ap-southeast-1': 'ami-60989c32',
+                 'ap-southeast-2': 'ami-07e3993d',
+                 'eu-central-1': 'ami-b092aaad',
+                 'eu-west-1': 'ami-0ed89d79',
+                 'sa-east-1': 'ami-a5bd3fb8',
+                 'us-east-1': 'ami-8b9a63e0',
+                 'us-west-1': 'ami-a5d621e1',
+                 'us-west-2': 'ami-3d56520d'},
+    'rhel-6': {'ap-northeast-1': 'ami-78379d78',
+               'ap-southeast-1': 'ami-faedeea8',
+               'ap-southeast-2': 'ami-7f0d4b45',
+               'eu-central-1': 'ami-8e96ac93',
+               'eu-west-1': 'ami-78d29c0f',
+               'sa-east-1': 'ami-d1d35ccc',
+               'us-east-1': 'ami-0d28fe66',
+               'us-west-1': 'ami-5b8a781f',
+               'us-west-2': 'ami-75f3f145'},
+    'rhel-7': {'ap-northeast-1': 'ami-35556534',
+               'ap-southeast-1': 'ami-941031c6',
+               'ap-southeast-2': 'ami-83e08db9',
+               'eu-central-1': 'ami-e25e6cff',
+               'eu-west-1': 'ami-8cff51fb',
+               'sa-east-1': 'ami-595ce844',
+               'us-east-1': 'ami-a8d369c0',
+               'us-west-1': 'ami-33cdd876',
+               'us-west-2': 'ami-99bef1a9'},
+    'ubuntu-12-04': {'ap-northeast-1': 'ami-88bca789',
+                     'ap-southeast-1': 'ami-74c6ec26',
+                     'ap-southeast-2': 'ami-33d4a009',
+                     'eu-central-1': 'ami-9cc4f681',
+                     'eu-west-1': 'ami-ff810f88',
+                     'sa-east-1': 'ami-57f14e4a',
+                     'us-east-1': 'ami-427a392a',
+                     'us-west-1': 'ami-82bba3c7',
+                     'us-west-2': 'ami-2b471c1b'},
+    'ubuntu-14-04': {'ap-northeast-1': 'ami-936d9d93',
+                     'ap-southeast-1': 'ami-96f1c1c4',
+                     'ap-southeast-2': 'ami-69631053',
+                     'eu-central-1': 'ami-accff2b1',
+                     'eu-west-1': 'ami-47a23a30',
+                     'sa-east-1': 'ami-4d883350',
+                     'us-east-1': 'ami-d05e75b8',
+                     'us-west-1': 'ami-df6a8b9b',
+                     'us-west-2': 'ami-5189a661'}
+}
