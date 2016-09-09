0e12f32912679bf50339e86e012b129540d47c59
Michael Ellenburg <michael.ellenburg@mesosphere.io>
2016-09-09 00:33:28 -0700

    Ensure that test applications close themselves

--- packages/dcos-integration-test/extra/test_service_discovery.py
+++ packages/dcos-integration-test/extra/test_service_discovery.py
@@ -72,59 +72,53 @@ def _service_discovery_test(cluster, docker_network_bridge):
 
     app_definition["constraints"] = [["hostname", "UNIQUE"], ]
 
-    service_points = cluster.deploy_marathon_app(app_definition)
+    with cluster.marathon_deploy_and_cleanup(app_definition) as service_points:
+        # Verify if Mesos-DNS agrees with Marathon:
+        @retrying.retry(wait_fixed=1000,
+                        stop_max_delay=MESOS_DNS_ENTRY_UPDATE_TIMEOUT * 1000,
+                        retry_on_result=lambda ret: ret is None,
+                        retry_on_exception=lambda x: False)
+        def _pool_for_mesos_dns():
+            r = cluster.get('/mesos_dns/v1/services/_{}._tcp.marathon.mesos'.format(
+                app_definition['id'].lstrip('/')))
+            assert r.status_code == 200
 
-    # Verify if Mesos-DNS agrees with Marathon:
-    @retrying.retry(wait_fixed=1000,
-                    stop_max_delay=MESOS_DNS_ENTRY_UPDATE_TIMEOUT * 1000,
-                    retry_on_result=lambda ret: ret is None,
-                    retry_on_exception=lambda x: False)
-    def _pool_for_mesos_dns():
-        r = cluster.get('/mesos_dns/v1/services/_{}._tcp.marathon.mesos'.format(
-                        app_definition['id'].lstrip('/')))
-        assert r.status_code == 200
+            r_data = r.json()
+            if r_data == [{'host': '', 'port': '', 'service': '', 'ip': ''}] or len(r_data) < len(service_points):
+                logging.info("Waiting for Mesos-DNS to update entries")
+                return None
+            else:
+                logging.info("Mesos-DNS entries have been updated!")
+                return r_data
+
+        try:
+            r_data = _pool_for_mesos_dns()
+        except retrying.RetryError:
+            msg = "Mesos DNS has failed to update entries in {} seconds."
+            pytest.fail(msg.format(MESOS_DNS_ENTRY_UPDATE_TIMEOUT))
+
+        marathon_provided_servicepoints = sorted((x.host, x.port) for x in service_points)
+        mesosdns_provided_servicepoints = sorted((x['ip'], int(x['port'])) for x in r_data)
+        assert marathon_provided_servicepoints == mesosdns_provided_servicepoints
+
+        # Verify if containers themselves confirm what Marathon says:
+        payload = {"reflector_ip": service_points[1].host,
+                   "reflector_port": service_points[1].port}
+        r = requests.post('http://{}:{}/your_ip'.format(
+            service_points[0].host, service_points[0].port), payload)
+        if r.status_code != 200:
+            msg = "Test server replied with non-200 reply: '{status_code} {reason}. "
+            msg += "Detailed explanation of the problem: {text}"
+            pytest.fail(msg.format(status_code=r.status_code, reason=r.reason, text=r.text))
 
         r_data = r.json()
-        if r_data == [{'host': '', 'port': '', 'service': '', 'ip': ''}] or \
-                len(r_data) < len(service_points):
-            logging.info("Waiting for Mesos-DNS to update entries")
-            return None
-        else:
-            logging.info("Mesos-DNS entries have been updated!")
-            return r_data
-
-    try:
-        r_data = _pool_for_mesos_dns()
-    except retrying.RetryError:
-        msg = "Mesos DNS has failed to update entries in {} seconds."
-        pytest.fail(msg.format(MESOS_DNS_ENTRY_UPDATE_TIMEOUT))
-
-    marathon_provided_servicepoints = sorted((x.host, x.port) for x in service_points)
-    mesosdns_provided_servicepoints = sorted((x['ip'], int(x['port'])) for x in r_data)
-    assert marathon_provided_servicepoints == mesosdns_provided_servicepoints
-
-    # Verify if containers themselves confirm what Marathon says:
-    payload = {"reflector_ip": service_points[1].host,
-               "reflector_port": service_points[1].port}
-    r = requests.post('http://{}:{}/your_ip'.format(service_points[0].host,
-                                                    service_points[0].port),
-                      payload)
-    if r.status_code != 200:
-        msg = "Test server replied with non-200 reply: '{status_code} {reason}. "
-        msg += "Detailed explanation of the problem: {text}"
-        pytest.fail(msg.format(status_code=r.status_code, reason=r.reason,
-                               text=r.text))
-
-    r_data = r.json()
-    assert r_data['reflector_uuid'] == test_uuid
-    assert r_data['test_uuid'] == test_uuid
-    if len(cluster.slaves) >= 2:
-        # When len(slaves)==1, we are connecting through docker-proxy using
-        # docker0 interface ip. This makes this assertion useless, so we skip
-        # it and rely on matching test uuid between containers only.
-        assert r_data['my_ip'] == service_points[0].host
-
-    cluster.destroy_marathon_app(app_definition['id'])
+        assert r_data['reflector_uuid'] == test_uuid
+        assert r_data['test_uuid'] == test_uuid
+        if len(cluster.slaves) >= 2:
+            # When len(slaves)==1, we are connecting through docker-proxy using
+            # docker0 interface ip. This makes this assertion useless, so we skip
+            # it and rely on matching test uuid between containers only.
+            assert r_data['my_ip'] == service_points[0].host
 
 
 def test_if_service_discovery_works_docker_bridged_network(cluster):
@@ -148,31 +142,28 @@ def test_if_search_is_working(cluster):
     """
     # Launch the app
     app_definition, test_uuid = cluster.get_test_app()
-    service_points = cluster.deploy_marathon_app(app_definition)
+    with cluster.marathon_deploy_and_cleanup(app_definition) as service_points:
+        # Get the status
+        r = requests.get('http://{}:{}/dns_search'.format(service_points[0].host,
+                                                          service_points[0].port))
+        if r.status_code != 200:
+            msg = "Test server replied with non-200 reply: '{0} {1}. "
+            msg += "Detailed explanation of the problem: {2}"
+            pytest.fail(msg.format(r.status_code, r.reason, r.text))
 
-    # Get the status
-    r = requests.get('http://{}:{}/dns_search'.format(service_points[0].host,
-                                                      service_points[0].port))
-    if r.status_code != 200:
-        msg = "Test server replied with non-200 reply: '{0} {1}. "
-        msg += "Detailed explanation of the problem: {2}"
-        pytest.fail(msg.format(r.status_code, r.reason, r.text))
+        r_data = r.json()
 
-    r_data = r.json()
+        # Make sure we hit the app we expected
+        assert r_data['test_uuid'] == test_uuid
 
-    # Make sure we hit the app we expected
-    assert r_data['test_uuid'] == test_uuid
+        expected_error = {'error': '[Errno -2] Name or service not known'}
 
-    expected_error = {'error': '[Errno -2] Name or service not known'}
-
-    # Check that result matches expectations for this cluster
-    if cluster.dns_search_set:
-        assert r_data['search_hit_leader'] in cluster.masters
-        assert r_data['always_hit_leader'] in cluster.masters
-        assert r_data['always_miss'] == expected_error
-    else:  # No dns search, search hit should miss.
-        assert r_data['search_hit_leader'] == expected_error
-        assert r_data['always_hit_leader'] in cluster.masters
-        assert r_data['always_miss'] == expected_error
-
-    cluster.destroy_marathon_app(app_definition['id'])
+        # Check that result matches expectations for this cluster
+        if cluster.dns_search_set:
+            assert r_data['search_hit_leader'] in cluster.masters
+            assert r_data['always_hit_leader'] in cluster.masters
+            assert r_data['always_miss'] == expected_error
+        else:  # No dns search, search hit should miss.
+            assert r_data['search_hit_leader'] == expected_error
+            assert r_data['always_hit_leader'] in cluster.masters
+            assert r_data['always_miss'] == expected_error
