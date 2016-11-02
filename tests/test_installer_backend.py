5610ffaa92916f06dc6b40303d8f8eedb1b074c6
Albert Strasheim <albert@mesosphere.com>
2016-11-02 12:58:41 -0700

    fix test_version for release branch

--- tests/test_installer_backend.py
+++ tests/test_installer_backend.py
@@ -44,7 +44,7 @@ def test_version(monkeypatch):
     monkeypatch.setenv('BOOTSTRAP_VARIANT', 'some-variant')
     version_data = subprocess.check_output(['dcos_installer', '--version']).decode()
     assert json.loads(version_data) == {
-        'version': '1.9-dev',
+        'version': '1.8.7',
         'variant': 'some-variant'
     }
 
