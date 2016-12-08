47fcc8524b666d5355103d53176da1f002ecb444
Jie Yu <yujie.jay@gmail.com>
2016-12-08 09:59:29 -0800

    Added 'posix/rlimits' to default Mesos isolator list.
    This isolator enables the POSIX rlimits support in Mesos. It allows
    frameworks to change the default resource limits (both hard and soft)
    for their containers, similar to Docker `--ulimit` flag.
    
    Some data services like Elastic Search or Cassandra typically require
    a higher than normal resource limit for their containers, and will do
    a check during startup to see if those limits are set properly. This
    support in Mesos will unblock them running on DC/OS.

--- gen/calc.py
+++ gen/calc.py
@@ -362,7 +362,7 @@ def calculate_config_yaml(user_arguments):
 
 def calculate_mesos_isolation(enable_gpu_isolation):
     isolators = ('cgroups/cpu,cgroups/mem,disk/du,network/cni,filesystem/linux,'
-                 'docker/runtime,docker/volume,volume/sandbox_path')
+                 'docker/runtime,docker/volume,volume/sandbox_path,posix/rlimits')
     if enable_gpu_isolation == 'true':
         isolators += ',cgroups/devices,gpu/nvidia'
     return isolators
