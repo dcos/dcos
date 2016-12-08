acfaa3fd3d696da0042b9c6ac4e8fad61ae605ca
Adam B <adam@mesosphere.io>
2016-12-07 18:38:40 -0800

    Debug test should handle streaming chunked recordio responses.

--- packages/dcos-integration-test/extra/test_mesos.py
+++ packages/dcos-integration-test/extra/test_mesos.py
@@ -1,112 +1,135 @@
+import json
 import logging
 import uuid
 
 import requests
 
 from test_util.marathon import get_test_app
+from test_util.recordio import Decoder, Encoder
+
+
+# Wrapper to post, log, and validate return status
+def post(url, headers, json=None, data=None, stream=False):
+    r = requests.post(url, headers=headers, json=json, data=data, stream=stream)
+    logging.info(
+        'Got %s with POST request to %s with headers %s and json data %s.',
+        r.status_code,
+        url,
+        headers,
+        json
+    )
+    assert r.status_code == 200
+    return r
+
+
+# Creates and yields the initial ATTACH_CONTAINER_INPUT message, then a data message,
+# then an empty data chunk to indicate end-of-stream.
+def input_streamer(nested_container_id):
+    encoder = Encoder(lambda s: bytes(json.dumps(s, ensure_ascii=False), "UTF-8"))
+    message = {
+        'type': 'ATTACH_CONTAINER_INPUT',
+        'attach_container_input': {
+            'type': 'CONTAINER_ID',
+            'container_id': nested_container_id}}
+    yield encoder.encode(message)
+
+    message['attach_container_input'] = {
+        'type': 'PROCESS_IO',
+        'process_io': {
+            'type': 'DATA',
+            'data': {'type': 'STDIN', 'data': 'meow'}}}
+    yield encoder.encode(message)
+
+    # Place an empty string to indicate EOF to the server and push
+    # 'None' to our queue to indicate that we are done processing input.
+    message['attach_container_input']['process_io']['data']['data'] = ''
+    yield encoder.encode(message)
 
 
 def test_if_marathon_app_can_be_debugged(cluster):
-    def post(url, data):
-        r = requests.post(url, json=data)
-        logging.info(
-            'Got %s with POST request to %s with %s. Response: \n%s',
-            r.status_code,
-            url,
-            data,
-            r.text
-        )
-        assert r.status_code == 200
-
-    def find_container_id(state, app_id):
-        if 'frameworks' in state:
-            for framework in state['frameworks']:
-                # TODO: Skip anything that's not Marathon
-                if 'tasks' in framework:
-                    for task in framework['tasks']:
-                        if 'id' in task and app_id in task['id']:
-                            if 'statuses' in task and 'container_status' in task['statuses'][0]:
-                                if 'container_id' in task['statuses'][0]['container_status']:
-                                    container_status = task['statuses'][0]['container_status']
-                                    if 'container_id' in container_status:
-                                        if 'value' in container_status['container_id']:
-                                            return container_status['container_id']['value']
-
-    def find_agent_id(state, app_id):
-        if 'frameworks' in state:
-            for framework in state['frameworks']:
-                # TODO: Skip anything that's not Marathon
-                if 'tasks' in framework:
-                    for task in framework['tasks']:
-                        if 'id' in task and app_id in task['id']:
-                            if 'slave_id' in task:
-                                return task['slave_id']
-
-    def find_agent_hostname(state, agent_id):
-        if 'slaves' in state:
-            for agent in state['slaves']:
-                if 'id' in agent and agent['id'] == agent_id and 'hostname' in agent:
-                    return agent['hostname']
-
     # Launch a basic marathon app (no image), so we can debug into it!
+    # Cannot use deploy_and_cleanup because we must attach to a running app/task/container.
     app, test_uuid = get_test_app()
-    test_app_id = 'integration-test-{}'.format(test_uuid)
+    app_id = 'integration-test-{}'.format(test_uuid)
     cluster.marathon.deploy_app(app)
 
-    # Find the agent_id and container_id from master state
+    # Fetch the mesos master state once the task is running
     master_state_url = 'http://{}:{}/state'.format(cluster.masters[0], 5050)
     r = requests.get(master_state_url)
-    logging.info('Got %s with request for %s. Response: \n%s', r.status_code, master_state_url, r.text)
+    logging.debug('Got %s with request for %s. Response: \n%s', r.status_code, master_state_url, r.text)
     assert r.status_code == 200
     state = r.json()
 
-    container_id = find_container_id(state, test_app_id)
-    agent_id = find_agent_id(state, test_app_id)
-    agent_hostname = find_agent_hostname(state, agent_id)
+    # Find the agent_id and container_id from master state
+    container_id = None
+    agent_id = None
+    for framework in state['frameworks']:
+        for task in framework['tasks']:
+            if app_id in task['id']:
+                container_id = task['statuses'][0]['container_status']['container_id']['value']
+                agent_id = task['slave_id']
+    if container_id is None:
+        raise Exception('Container ID not found for instance of app_id {}'.format(app_id))
+    if agent_id is None:
+        raise Exception('Agent ID not found for instance of app_id {}'.format(app_id))
+
+    # Find hostname and URL from agent_id
+    agent_hostname = None
+    for agent in state['slaves']:
+        if agent['id'] == agent_id:
+            agent_hostname = agent['hostname']
+    if agent_hostname is None:
+        raise Exception('Agent hostname not found for agent_id {}'.format(agent_id))
     agent_v1_url = 'http://{}:{}/api/v1'.format(agent_hostname, 5051)
-    logging.info('Located %s with containerID %s on agent %s', test_app_id, container_id, agent_hostname)
+    logging.debug('Located %s with containerID %s on agent %s', app_id, container_id, agent_hostname)
 
-    # Attach to output stream of container
-    container_id_data = {'value': '%s' % container_id}
-    attach_out_data = {'type': 'ATTACH_CONTAINER_OUTPUT', 'attach_container_output': {}}
-    attach_out_data['attach_container_output']['container_id'] = container_id_data
-    logging.info('Making POST call to %s with: %s', agent_v1_url, attach_out_data)
-    # r = post(agent_v1_url, attach_out_data)
-    # logging.info('For %s call, got %s response: \n%s', attach_out_data['type'], r.status_code, r.text)
-    # assert r.status_code == 200
-    # TODO: verify some output
+    # Prepare nested container id data
+    nested_container_id = {
+        'value': 'debug-%s' % str(uuid.uuid4()),
+        'parent': {'value': '%s' % container_id}}
 
-    # Prepare nested container id
-    nested_container_id = {'value': 'debug-%s' % str(uuid.uuid4())}
-    nested_container_id['parent'] = container_id_data
-    logging.info('Creating nested container session: %s', nested_container_id)
+    # Launch debug session and attach to output stream of debug container
+    output_headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json+recordio',
+        'Connection': 'keep-alive'
+    }
+    lncs_data = {
+        'type': 'LAUNCH_NESTED_CONTAINER_SESSION',
+        'launch_nested_container_session': {
+            'command': {'value': 'cat'},
+            'container_id': nested_container_id}}
+    launch_output = post(agent_v1_url, output_headers, json=lncs_data, stream=True)
 
-    # Prepare POST json with nested_container_id for each call
-    lncs_data = {'type': 'LAUNCH_NESTED_CONTAINER_SESSION', 'launch_nested_container_session': {}}
-    lncs_data['launch_nested_container_session']['command'] = {'value': 'echo echo'}
-    lncs_data['launch_nested_container_session']['container_id'] = nested_container_id
-    attach_in_data = {'type': 'ATTACH_CONTAINER_INPUT', 'attach_container_input': {}}
-    attach_in_data['attach_container_input']['type'] = 'CONTAINER_ID'
-    attach_in_data['attach_container_input']['container_id'] = nested_container_id
-    # attach_out_data = {'type': 'ATTACH_CONTAINER_OUTPUT', 'attach_container_output': {}}
-    attach_out_data['attach_container_output']['container_id'] = nested_container_id
+    # Attach to output stream of nested container
+    attach_out_data = {
+        'type': 'ATTACH_CONTAINER_OUTPUT',
+        'attach_container_output': {'container_id': nested_container_id}}
+    attached_output = post(agent_v1_url, output_headers, json=attach_out_data, stream=True)
 
-    # Launch debug session
-    r = post(agent_v1_url, lncs_data)
-    logging.info('For %s call, got %s response: \n%s', lncs_data['type'], r.status_code, r.text)
-    assert r.status_code == 200
-    # TODO: verify more of the response contents?
+    # Attach to input stream of debug container and stream a message
+    input_headers = {
+        'Content-Type': 'application/json+recordio',
+        'Accept': 'application/json',
+        'Connection': 'keep-alive',
+        'Transfer-Encoding': 'chunked'
+    }
+    post(agent_v1_url, input_headers, data=input_streamer(nested_container_id))
 
-    # Attach to output stream of debug container
-    # r = post(agent_v1_url, attach_out_data)
-    # logging.info('For %s call, got %s response: \n%s', attach_out_data['type'], r.status_code, r.text)
-    # assert r.status_code == 200
-    # TODO: verify some output
+    # Verify the streamed output from the launch session
+    decoder = Decoder(lambda s: json.loads(s.decode("UTF-8")))
+    for chunk in launch_output.iter_content():
+        for r in decoder.decode(chunk):
+            if r['type'] == 'DATA':
+                logging.debug('Extracted data chunk: %s', r['data'])
+                assert r['data']['data'] == 'meow', 'Output did not match expected'
 
-    # Attach to input stream of debug container
-    # r = post(agent_v1_url, attach_in_data)
-    # logging.info('For %s call, got %s response: \n%s', attach_in_data['type'], r.status_code, r.text)
-    # assert r.status_code == 200
-    # TODO: input something and verify it
+    # Verify the message from the attached output stream
+    for chunk in attached_output.iter_content():
+        for r in decoder.decode(chunk):
+            if r['type'] == 'DATA':
+                logging.debug('Extracted data chunk: %s', r['data'])
+                assert r['data']['data'] == 'meow', 'Output did not match expected'
 
-    cluster.marathon.destroy_app(test_app_id)
+    # Destroy the app and the task's containers now that we're done with it
+    cluster.marathon.destroy_app(app_id)
