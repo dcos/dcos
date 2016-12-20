e615b5d0c9783b6f9c4a3b0a005dfc891e9d272b
Senthil Kumaran <skumaran@mesosphere.com>
2016-12-20 11:36:05 -0800

    Address review comments.

--- flake8_dcos_lint/check_rules.py
+++ flake8_dcos_lint/check_rules.py
@@ -6,5 +6,6 @@ CheckRegex = namedtuple("CheckRegex", "regex code reason")
 
 
 regex_rules = [
-    CheckRegex(re.compile(r'assert\s(\w+\.ok)$'), "D001", "Invalid assertion of type assert <symbol>.ok found.")
+    # Prefer asserting actual == expected, instead of asserting on .ok on a result object.
+    CheckRegex(re.compile(r'assert\s(\w+\.ok)$'), "D001", "Assertion of type assert <symbol>.ok detected.")
 ]
--- flake8_dcos_lint/setup.py
+++ flake8_dcos_lint/setup.py
@@ -1,3 +1,11 @@
+"""
+flake8 plugin for dcos code base.
+
+This is flake8 extension that provides custom code quality checks for DCOS project. The syntax rules can be added as
+Regex in check_rules.py module. New plugins can be introduced by following the flake8 extension guide
+(http://flake8.pycqa.org/en/latest/plugin-development/index.html). And they will automatically be run as part of the
+syntax-check.
+"""
 from __version__ import PLUGIN_NAME, PLUGIN_VERSION
 
 from setuptools import setup
