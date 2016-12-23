fdc4c8a51c0796e19d76a6cea7bc1cee54c53e63
Adam B <adam@mesosphere.io>
2016-12-22 21:22:48 -0800

    Clean up dockercfg remover test

--- packages/dcos-integration-test/extra/test_remove_dockercfg_hook.py
+++ packages/dcos-integration-test/extra/test_remove_dockercfg_hook.py
@@ -14,23 +14,16 @@ def test_remove_dockercfg_hook(cluster):
     """Test that the remove .dockercfg hook is working properly.
 
     If the hook is enabled, the test expects that the .dockercfg file
-    (which is downloaded as a uri via the Mesos fetcher) is removed.
+    (downloaded as a uri via the Mesos fetcher) is removed from the task's sandbox.
 
     """
 
-    # Create a one-off job checking that the fetched .dockercfg file is
-    # not in the sandbox
-    test_uuid = uuid.uuid4().hex
+    # Create a one-off job checking that the fetched .dockercfg file is not in the sandbox
     job = {
-        'id': 'integration-test--' + test_uuid,
+        'id': 'integration-test--' + uuid.uuid4().hex,
         'run': {
-            'cpus': 0.1,
-            'mem': 32,
-            'disk': 0,
-            'cmd': "test -f .dockercfg",
-            'artifacts': [{
-                'uri': "file:///opt/mesosphere/active/dcos-integration-test/.dockercfg"}]}}
-    try:
-        cluster.metronome_one_off(job)
-    except Exception as ex:
-        raise Exception(".dockercfg was not removed")
+            'cpus': 0.1, 'mem': 32,
+            'cmd': "test ! -f .dockercfg",
+            'artifacts': [{'uri': "file:///opt/mesosphere/active/dcos-integration-test/.dockercfg"}]}}
+    removed = cluster.metronome_one_off(job)
+    assert removed, 'dockercfg was not removed from the sandbox'
