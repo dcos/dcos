7e8f55c0addaae161254e1bbcb04aa92a85631e2
Gustav Paul <gpaul@mesosphere.io>
2017-01-31 21:20:01 +0100

    pkgpanda-api: move ExecStart command into its own shell script
    Systemd's quotes, strings and escaping isn't like that of bash and
    this unit fails to pass systemd-analyse verify.

---
+++ packages/pkgpanda-api/extra/pkgpanda-api.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -xe
+
+exec /opt/mesosphere/bin/gunicorn --worker-class=sync \
+    --workers=1 \
+    --threads=10 \
+    --bind=unix:/run/dcos/pkgpanda-api.sock \
+    --timeout=10 \
+    --graceful-timeout=30 \
+    --name Pkgpanda\ API \
+    --access-logfile - \
+    --access-logformat '%({X-Forwarded-For}i)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" (%(L)s s)' \
+    --error-logfile - \
+    --log-level=info \
+    pkgpanda.http:app
