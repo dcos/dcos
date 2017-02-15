78947f6ef37aeb88af93b07367851d2b78a851a0
Senthil Kumaran <skumaran@mesosphere.com>
2017-02-15 12:53:43 -0800

    test upgrade vpc.

--- test_util/test_upgrade_vpc.py
+++ test_util/test_upgrade_vpc.py
@@ -356,7 +356,7 @@ class VpcClusterUpgradeTest:
             for k, v in os.environ.items():
                 if k.startswith(prefix):
                     add_env.append(k.replace(prefix, '') + '=' + v)
-            test_cmd = ' '.join(add_env) + 'py.test -vv -s -rs ' + os.getenv('CI_FLAGS', '')
+            test_cmd = ' '.join(add_env) + ' ' + 'py.test -vv -s -rs ' + os.getenv('CI_FLAGS', '')
             result = test_util.cluster.run_integration_tests(cluster, test_cmd=test_cmd)
 
         if result == 0:
