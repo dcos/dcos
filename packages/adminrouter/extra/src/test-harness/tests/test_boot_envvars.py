b754b60664c08ef7585dd16a75a452899818d6af
Pawel Rozlach <pawel.rozlach@gmail.com>
2017-03-09 14:27:04 +0100

    Admin Router: refactor TestSecretKeyFilePathEnvVarBehaviour tests

--- packages/adminrouter/extra/src/test-harness/tests/test_boot_envvars.py
+++ packages/adminrouter/extra/src/test-harness/tests/test_boot_envvars.py
@@ -3,8 +3,9 @@
 import logging
 import pytest
 import requests
+import time
 
-from util import LineBufferFilter
+from util import LineBufferFilter, SearchCriteria, GuardedSubprocess
 
 log = logging.getLogger(__name__)
 
@@ -16,73 +17,53 @@ def empty_file(tmp_file):
     return tmp_file
 
 
-@pytest.fixture(scope='function')
-def ar_process_without_secret_key(nginx_class, request, empty_file):
-    """Provide a (master|agent) AR process which has `SECRET_KEY_FILE_PATH`
-       variable (empty|not-set), depending on the parameters passed to the
-       fixture."""
-    role = request.param[0]
-    use_empty = request.param[1]
-
-    if use_empty:
-        nginx = nginx_class(role=role, secret_key_file_path=empty_file)
-    else:
-        nginx = nginx_class(role=role, secret_key_file_path=None)
-
-    nginx.start()
-
-    yield nginx
-
-    nginx.stop()
-
-
 class TestSecretKeyFilePathEnvVarBehaviour():
-    @pytest.mark.parametrize('ar_process_without_secret_key',
-                             [('master', False), ('agent', False)],
-                             indirect=['ar_process_without_secret_key'],
-                             )
-    def test_if_not_defining_the_var_is_handled(self,
-                                                ar_process_without_secret_key):
+    @pytest.mark.parametrize('role', ['master', 'agent'])
+    def test_if_not_defining_the_var_is_handled(self, nginx_class, role):
         # Scanning for the exact log entry is bad, but in this case - can't be
         # avoided.
         filter_regexp = 'SECRET_KEY_FILE_PATH not set.'
+        ar = nginx_class(role=role, secret_key_file_path=None)
 
-        lbf = LineBufferFilter(filter_regexp,
-                               line_buffer=ar_process_without_secret_key.stderr_line_buffer)
-
-        lbf.scan_log_buffer()
+        with GuardedSubprocess(ar):
+            lbf = LineBufferFilter(filter_regexp,
+                                   line_buffer=ar.stderr_line_buffer)
+            lbf.scan_log_buffer()
 
         assert lbf.all_found is True
 
-    @pytest.mark.parametrize('ar_process_without_secret_key',
-                             [('master', True), ('agent', True)],
-                             indirect=['ar_process_without_secret_key'],
-                             )
-    def test_if_var_pointing_to_empty_file_is_handled(self,
-                                                      ar_process_without_secret_key):
+    @pytest.mark.parametrize('role', ['master', 'agent'])
+    def test_if_var_pointing_to_empty_file_is_handled(
+            self, nginx_class, role, empty_file):
         # Scanning for the exact log entry is bad, but in this case - can't be
         # avoided.
         filter_regexp = 'Secret key not set or empty string.'
+        ar = nginx_class(role=role, secret_key_file_path=empty_file)
 
-        lbf = LineBufferFilter(filter_regexp,
-                               line_buffer=ar_process_without_secret_key.stderr_line_buffer)
+        with GuardedSubprocess(ar):
+            lbf = LineBufferFilter(filter_regexp,
+                                   line_buffer=ar.stderr_line_buffer)
 
-        lbf.scan_log_buffer()
+            lbf.scan_log_buffer()
 
         assert lbf.all_found is True
 
     # TODO: ATM in Agent-Open there are no paths we can test auth with
-    @pytest.mark.parametrize('ar_process_without_secret_key',
+    @pytest.mark.parametrize('role,use_empty',
                              [('master', False), ('master', True)],
-                             indirect=['ar_process_without_secret_key'],
                              )
-    def test_if_bad_var_fails_all_requests(self,
-                                           ar_process_without_secret_key,
-                                           valid_user_header):
+    def test_if_bad_var_fails_all_requests(
+            self, nginx_class, role, use_empty, empty_file, valid_user_header):
 
-        url = ar_process_without_secret_key.make_url_from_path()
-        resp = requests.get(url,
-                            allow_redirects=False,
-                            headers=valid_user_header)
+        if use_empty:
+            ar = nginx_class(role=role, secret_key_file_path=empty_file)
+        else:
+            ar = nginx_class(role=role, secret_key_file_path=None)
+        url = ar.make_url_from_path()
+
+        with GuardedSubprocess(ar):
+            resp = requests.get(url,
+                                allow_redirects=False,
+                                headers=valid_user_header)
 
         assert resp.status_code == 401
