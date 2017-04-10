d1d3d88203add6acccfb21b03120ccb6659baf80
Pawel Rozlach <pawel.rozlach@gmail.com>
2017-04-11 00:24:13 +0200

    Admin Router: Consolidate service.lua code

--- packages/adminrouter/extra/src/master/service.lua
+++ packages/adminrouter/extra/src/master/service.lua
@@ -1,8 +1,30 @@
-local util = require "master.util"
 local url = require "master.url"
+local cjson_safe = require "cjson.safe"
+
+function mesos_dns_get_srv(framework_name)
+    local res = ngx.location.capture(
+        "/internal/mesos_dns/v1/services/_" .. framework_name .. "._tcp.marathon.mesos")
+
+    if res.truncated then
+        -- Remote connection dropped prematurely or timed out.
+        ngx.log(ngx.ERR, "Request to Mesos DNS failed.")
+        return nil
+    end
+    if res.status ~= 200 then
+        ngx.log(ngx.ERR, "Mesos DNS response status: " .. res.status)
+        return nil
+    end
+
+    local records, err = cjson_safe.decode(res.body)
+    if not records then
+        ngx.log(ngx.ERR, "Cannot decode JSON: " .. err)
+        return nil
+    end
+    return records
+end
 
 function gen_serviceurl(service_name)
-    local records = util.mesos_dns_get_srv(service_name)
+    local records = mesos_dns_get_srv(service_name)
     if records == nil then
         ngx.status = ngx.HTTP_SERVICE_UNAVAILABLE
         ngx.say("503 Service Unavailable: MesosDNS request has failed")
