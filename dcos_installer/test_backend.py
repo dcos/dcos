7c5460c0d61daecf91e3d11957a4d44487610b39
Michael Ellenburg <michael.ellenburg@mesosphere.io>
2017-04-25 12:06:32 -0700

    Bump DC/OS version to 1.9.1

--- dcos_installer/test_backend.py
+++ dcos_installer/test_backend.py
@@ -104,7 +104,7 @@ def test_version(monkeypatch):
     monkeypatch.setenv('BOOTSTRAP_VARIANT', 'some-variant')
     version_data = subprocess.check_output(['dcos_installer', '--version']).decode()
     assert json.loads(version_data) == {
-        'version': '1.9.0',
+        'version': '1.9.1',
         'variant': 'some-variant'
     }
 
