34e395d83ca4cbba03e8cb586234274479485a80
Jie Yu <yujie.jay@gmail.com>
2017-05-05 15:12:31 -0700

    Enable Linux Capabilities isolator in Mesos.
    This only enables the isolator. Since we don't set the
    `--allowed_capabilities` flag on the Mesos agent, the agent should
    behave the same as it is currently. However, this allows the framework
    to optionally use this feature by setting ContainerInfo properly.

--- gen/calc.py
+++ gen/calc.py
@@ -416,7 +416,7 @@ def calculate_config_yaml(user_arguments):
 def calculate_mesos_isolation(enable_gpu_isolation):
     isolators = ('cgroups/cpu,cgroups/mem,disk/du,network/cni,filesystem/linux,'
                  'docker/runtime,docker/volume,volume/sandbox_path,volume/secret,posix/rlimits,'
-                 'namespaces/pid,com_mesosphere_MetricsIsolatorModule')
+                 'namespaces/pid,linux/capabilities,com_mesosphere_MetricsIsolatorModule')
     if enable_gpu_isolation == 'true':
         isolators += ',cgroups/devices,gpu/nvidia'
     return isolators
