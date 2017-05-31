0029c0ed649af7e3460400764bad54ae1f017796
Pawel Rozlach <prozlach@mesosphere.io>
2017-05-31 15:55:25 +0200

    Admin Router: Fix bug in X-Accel-Buffering passing, adjust tests

--- packages/adminrouter/extra/src/test-harness/tests/test_agent.py
+++ packages/adminrouter/extra/src/test-harness/tests/test_agent.py
@@ -1,26 +1,29 @@
 # Copyright (C) Mesosphere, Inc. See LICENSE file for details.
 
-import copy
-
 import pytest
 
-from generic_test_code.common import generic_upstream_headers_verify_test
+from generic_test_code.common import generic_response_headers_verify_test
 
 pytestmark = pytest.mark.usefixtures("agent_ar_process")
 
 
 class TestLogsEndpoint:
-    def test_if_upstream_headers_are_correct(self,
-                                             agent_ar_process,
-                                             valid_user_header):
+    def test_if_xaccel_header_is_passed_to_client_by_ar(
+            self,
+            agent_ar_process,
+            valid_user_header,
+            mocker):
 
         accel_buff_header = {"X-Accel-Buffering": "TEST"}
 
-        req_headers = copy.deepcopy(valid_user_header)
-        req_headers.update(accel_buff_header)
+        mocker.send_command(
+            endpoint_id='http:///run/dcos/dcos-log.sock',
+            func_name='set_response_headers',
+            aux_data=accel_buff_header,
+        )
 
-        generic_upstream_headers_verify_test(agent_ar_process,
-                                             req_headers,
+        generic_response_headers_verify_test(agent_ar_process,
+                                             valid_user_header,
                                              '/system/v1/logs/v1/foo/bar',
                                              assert_headers=accel_buff_header,
                                              )
