41df6947d05a0f4aad20512b65639474d3c8c838
Pawel Rozlach <prozlach@mesosphere.io>
2017-05-31 17:01:21 +0200

    Admin Router: Proper tests for disabling auth module/DCOS_OSS-1003

--- packages/adminrouter/extra/src/test-harness/tests/test_boot_envvars.py
+++ packages/adminrouter/extra/src/test-harness/tests/test_boot_envvars.py
@@ -337,3 +337,49 @@ class TestHostIPVarBehavriour:
             lbf.scan_log_buffer()
 
         assert lbf.extra_matches == {}
+
+
+class TestAuthModuleDisablingMaster:
+    @pytest.mark.parametrize(
+        "enable_keyword",
+        ["enabled", "true", "yes", "of_course", "make it so!",
+         "disabled", "no", "no way", "please no"])
+    def test_if_auth_module_is_enabled_by_unless_false_str_is_provided(
+            self, nginx_class, mocker, enable_keyword):
+        filter_regexp = {
+            'Activate authentication module.': SearchCriteria(1, True),
+        }
+        ar = nginx_class(auth_enabled=enable_keyword)
+        url = ar.make_url_from_path('/exhibitor/foo/bar')
+
+        with GuardedSubprocess(ar):
+            lbf = LineBufferFilter(filter_regexp,
+                                   line_buffer=ar.stderr_line_buffer)
+
+            resp = requests.get(url,
+                                allow_redirects=False)
+
+            assert resp.status_code == 401
+            lbf.scan_log_buffer()
+
+        assert lbf.extra_matches == {}
+
+    def test_if_auth_module_can_be_disabled(self, nginx_class, mocker):
+        filter_regexp = {
+            ("ADMINROUTER_ACTIVATE_AUTH_MODULE set to `false`. "
+             "Deactivate authentication module."): SearchCriteria(1, True),
+        }
+        ar = nginx_class(auth_enabled='false')
+        url = ar.make_url_from_path('/exhibitor/foo/bar')
+
+        with GuardedSubprocess(ar):
+            lbf = LineBufferFilter(filter_regexp,
+                                   line_buffer=ar.stderr_line_buffer)
+
+            resp = requests.get(url,
+                                allow_redirects=False)
+
+            assert resp.status_code == 200
+            lbf.scan_log_buffer()
+
+        assert lbf.extra_matches == {}
