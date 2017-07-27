77fe1fc02e77b88c7fb3b84a858d1276fde2fdcd
Michael Ellenburg <michael.ellenburg@mesosphere.io>
2017-07-26 17:36:39 -0700

    Setup logging with dcos-test-utils

--- packages/dcos-integration-test/extra/api_session_fixture.py
+++ packages/dcos-integration-test/extra/api_session_fixture.py
@@ -2,21 +2,20 @@
 DcosApiSession object that will be injected into the pytest 'dcos_api_session' fixture
 via the make_session_fixture() method
 """
-from dcos_test_utils.dcos_api_session import DcosApiSession, DcosUser
-from dcos_test_utils.helpers import CI_CREDENTIALS
 
+from dcos_test_utils import dcos_api, helpers
 from test_helpers import expanded_config
 
 
 def make_session_fixture():
-    args = DcosApiSession.get_args_from_env()
+    args = dcos_api.DcosApiSession.get_args_from_env()
 
     exhibitor_admin_password = None
     if expanded_config['exhibitor_admin_password_enabled'] == 'true':
         exhibitor_admin_password = expanded_config['exhibitor_admin_password']
 
-    dcos_api_session = DcosApiSession(
-        auth_user=DcosUser(CI_CREDENTIALS),
+    dcos_api_session = dcos_api.DcosApiSession(
+        auth_user=dcos_api.DcosUser(helpers.CI_CREDENTIALS),
         exhibitor_admin_password=exhibitor_admin_password,
         **args)
     dcos_api_session.wait_for_dcos()
