df989b84c44fdc383a0066a7eeb1f5d0d62267fc
Ken Sipe <kensipe@gmail.com>
2017-09-20 16:49:40 -0500

    added all the env vars for marathon.  now aligned with the enterprise startup and packaging

--- packages/marathon/extra/marathon.sh
+++ packages/marathon/extra/marathon.sh
@@ -5,29 +5,21 @@ export HOST_IP=$($MESOS_IP_DISCOVERY_COMMAND)
 export MARATHON_HOSTNAME=$HOST_IP
 export LIBPROCESS_IP=$HOST_IP
 
-JAVA_ARGS=
+MARATHON_EXTRA_ARGS="${MARATHON_EXTRA_ARGS-}"
 
-EXTRA_ARGS="${MARATHON_EXTRA_ARGS-} --mesos_user ${MESOS_USER} --mesos_authentication_principal dcos_marathon"
-if [ "${MESOS_FRAMEWORK_AUTHN-}" = "true" ]; then
-    EXTRA_ARGS="${EXTRA_ARGS} --mesos_authentication"
-fi
-
-export -n MARATHON_EXTRA_ARGS
 exec $PKG_PATH/marathon/bin/marathon \
-    -Duser.dir=/var/lib/dcos/marathon \
-    -J-server \
-    -J-verbose:gc \
-    -J-XX:+PrintGCDetails \
-    -J-XX:+PrintGCTimeStamps \
+    -Duser.dir=$MARATHON_USER_DIR \
+    $MARATHON_JVM_ARGS \
     --master $MARATHON_MESOS_MASTER_URL \
-    --default_accepted_resource_roles "*" \
-    --mesos_role "slave_public" \
-    --max_instances_per_offer 100 \
-    --task_launch_timeout 86400000 \
-    --decline_offer_duration 300000 \
+    --default_accepted_resource_roles $MARATHON_ACCEPTED_ROLES \
+    --mesos_role $MARATHON_MESOS_ROLE \
+    --max_instances_per_offer $MARATHON_MAX_INSTANCES_PER_OFFER \
+    --task_launch_timeout $MARATHON_TASK_LAUNCH_TIMEOUT \
+    --decline_offer_duration $MARATHON_DECLINE_OFFER_DURATION \
     --revive_offers_for_new_apps \
     --zk_compression \
-    --mesos_leader_ui_url "/mesos" \
-    --enable_features "vips,task_killing,external_volumes,gpu_resources" \
-    --mesos_authentication_principal "dcos_marathon" \
-    --mesos_user $MARATHON_MESOS_USER
+    --mesos_leader_ui_url $MARATHON_LEADER_UI_URL \
+    --enable_features $MARATHON_FEATURES \
+    --mesos_authentication_principal $MARATHON_PRINCIPAL \
+    --mesos_user $MARATHON_MESOS_USER \
+    $MARATHON_EXTRA_ARGS
