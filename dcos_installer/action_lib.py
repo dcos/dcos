ec146bab9a1080aba09fe251a138e5c4318511d7
Karl Isenberg <karl@mesosphere.io>
2017-10-11 17:24:02 -0700

    Fix Docker version validation

--- dcos_installer/action_lib.py
+++ dcos_installer/action_lib.py
@@ -415,7 +415,7 @@ fi
 echo "Validating kernel version..."
 kernel_major_version="$(uname -r | sed -e 's/\([0-9][0-9]*\).*/\1/')"
 kernel_minor_version="$(uname -r | sed -e "s/${kernel_major_version}\.\([0-9][0-9]*\).*/\1/")"
-if [[ ${kernel_major_version} -lt 3 || ${kernel_minor_version} -lt 10 ]]; then
+if [[ ${docker_major_version} -lt 1 || ${docker_major_version} -eq 1 && ${kernel_minor_version} -lt 11 ]]; then
   echo "Error: Kernel version ${kernel_major_version}.${kernel_minor_version} is not supported. Only >= 3.10 is supported." >&2
   exit 1
 fi
