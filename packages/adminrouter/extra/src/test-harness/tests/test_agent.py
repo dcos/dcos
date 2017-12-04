66482cc5bf9ac824d8480e157f91438481526e1d
Pawel Rozlach <prozlach@mesosphere.io>
2017-12-04 22:05:16 +0100

    Admin Router: Prevent Agent AR from servin default Nginx homepage

--- packages/adminrouter/extra/src/test-harness/tests/test_agent.py
+++ packages/adminrouter/extra/src/test-harness/tests/test_agent.py
@@ -1,5 +1,8 @@
 # Copyright (C) Mesosphere, Inc. See LICENSE file for details.
 
+import pytest
+import requests
+
 from generic_test_code.common import generic_response_headers_verify_test
 
 
@@ -24,3 +27,21 @@ class TestLogsEndpoint:
             '/system/v1/logs/foo/bar',
             assert_headers=accel_buff_header,
             )
+
+
+class TestAgentMisc:
+    @pytest.mark.parametrize('path', ['/', '/foo', '/foo/bar'])
+    def test_if_document_root_is_not_served_by_the_agent_ar(
+            self,
+            agent_ar_process,
+            valid_user_header,
+            path):
+
+        url = agent_ar_process.make_url_from_path(path)
+        resp = requests.get(
+            url,
+            allow_redirects=False,
+            headers=valid_user_header
+            )
+
+        assert resp.status_code == 404
