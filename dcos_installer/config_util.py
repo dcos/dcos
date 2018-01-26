29c7a75420ec5fc198c4641ab8504c4c64b8ced2
Michael Ellenburg <michael.ellenburg@mesosphere.io>
2018-01-25 16:17:27 -0800

    Update fetch_artifacts

--- dcos_installer/config_util.py
+++ dcos_installer/config_util.py
@@ -93,31 +93,22 @@ def do_move_atomic(src_dir, dest_dir, filenames):
         rollback()
 
 
-def fetch_artifacts(bootstrap_id, cluster_packages, config_package_ids):
-    filenames = [
-        "bootstrap/{}.bootstrap.tar.xz".format(bootstrap_id),
-        "bootstrap/{}.active.json".format(bootstrap_id)
-    ] + sorted(
-        # Onprem config packages are created by genconf. They aren't available in the cache.
-        info['filename'] for info in cluster_packages.values() if info['id'] not in config_package_ids
-    )
-    dest_dir = SERVE_DIR
-    container_cache_dir = ARTIFACT_DIR
+def fetch_artifacts(filenames, src_dir, dest_dir):
 
     # If all the targets already exist, no-op
     dest_files = [dest_dir + '/' + filename for filename in filenames]
     if all(map(os.path.exists, dest_files)):
         return
 
-    # Make sure the internal cache files exist
-    src_files = [container_cache_dir + '/' + filename for filename in filenames]
+    # Make sure the source files exist
+    src_files = [src_dir + '/' + filename for filename in filenames]
     for filename in src_files:
         if not os.path.exists(filename):
             log.error("Internal Error: %s not found. Should have been in the installer container.", filename)
             raise FileNotFoundError(filename)
 
     subprocess.check_call(['mkdir', '-p', dest_dir])
-    do_move_atomic(container_cache_dir, dest_dir, filenames)
+    do_move_atomic(src_dir, dest_dir, filenames)
 
 
 def installer_latest_complete_artifact(variant_str):
