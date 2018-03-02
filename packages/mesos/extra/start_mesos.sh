c2a7a9ca1e2de2ea3ff2a295f74b7a66da6a776c
Michael Ellenburg <michael.ellenburg@mesosphere.io>
2018-03-01 16:16:18 -0800

    Only restart if unit is active, not enabled

--- packages/mesos/extra/start_mesos.sh
+++ packages/mesos/extra/start_mesos.sh
@@ -5,12 +5,12 @@ set -e
 function coreos_networkd_config() {
  network_config="/etc/systemd/network/dcos.network"
  sudo tee $network_config > /dev/null<<'EOF'
- [Match]
-  Type=bridge
-  Name=docker* m-* d-* vtep*
+[Match]
+Type=bridge
+Name=docker* m-* d-* vtep*
 
- [Link]
-  Unmanaged=yes
+[Link]
+Unmanaged=yes
 EOF
 }
 
@@ -19,7 +19,7 @@ if [[ "${distro}" == 'coreos' ]]; then
      if systemctl list-unit-files | grep systemd-networkd.service > /dev/null; then
        coreos_networkd_config
 
-       if systemctl is-enabled systemd-networkd > /dev/null; then
+       if systemctl is-active systemd-networkd > /dev/null; then
           sudo systemctl restart systemd-networkd
        fi
     fi
