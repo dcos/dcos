5489b9f68379a47e295f7190a7e801446602a636
Gauri Powale <gpowale@mesosphere.com>
2018-03-07 16:52:57 -0800

    Re-adjusted script to use Docker 1.12.6

--- cloud_images/centos7/install_prereqs.sh
+++ cloud_images/centos7/install_prereqs.sh
@@ -47,7 +47,7 @@ ExecStart=
 ExecStart=/usr/bin/dockerd --storage-driver=overlay
 EOF
 
-sudo yum install -y docker-engine-1.13.1 docker-engine-selinux-1.13.1
+sudo yum install -y docker-engine-1.12.6 docker-engine-selinux-1.12.6
 sudo systemctl start docker
 sudo systemctl enable docker
 
@@ -94,10 +94,10 @@ echo ">>> Removing tty requirement for sudo"
 sed -i'' -E 's/^(Defaults.*requiretty)/#\1/' /etc/sudoers
 
 echo ">>> Install Docker"
-curl -fLsSv --retry 20 -Y 100000 -y 60 -o /tmp/docker-engine-1.13.1-1.el7.centos.x86_64.rpm \
-  https://yum.dockerproject.org/repo/main/centos/7/Packages/docker-engine-1.13.1-1.el7.centos.x86_64.rpm
-curl -fLsSv --retry 20 -Y 100000 -y 60 -o /tmp/docker-engine-selinux-1.13.1-1.el7.centos.noarch.rpm \
-  https://yum.dockerproject.org/repo/main/centos/7/Packages/docker-engine-selinux-1.13.1-1.el7.centos.noarch.rpm
+curl -fLsSv --retry 20 -Y 100000 -y 60 -o /tmp/docker-engine-1.12.6-1.el7.centos.x86_64.rpm \
+  https://yum.dockerproject.org/repo/main/centos/7/Packages/docker-engine-1.12.6-1.el7.centos.x86_64.rpm
+curl -fLsSv --retry 20 -Y 100000 -y 60 -o /tmp/docker-engine-selinux-1.12.6-1.el7.centos.noarch.rpm \
+  https://yum.dockerproject.org/repo/main/centos/7/Packages/docker-engine-selinux-1.12.6-1.el7.centos.noarch.rpm
 yum -y localinstall /tmp/docker*.rpm || true
 systemctl enable docker
 
