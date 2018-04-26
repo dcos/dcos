1ffeb0bb56f2c773df1c706fd4f0516c0820ff90
Senthil Kumaran <skumaran@mesosphere.com>
2018-04-26 06:37:26 -0700

    Update version to 1.11.2

--- dcos_installer/test_backend.py
+++ dcos_installer/test_backend.py
@@ -83,7 +83,7 @@ def test_version(monkeypatch):
     monkeypatch.setenv('BOOTSTRAP_VARIANT', 'some-variant')
     version_data = subprocess.check_output(['dcos_installer', '--version']).decode()
     assert json.loads(version_data) == {
-        'version': '1.11.1',
+        'version': '1.11.2',
         'variant': 'some-variant'
     }
 
