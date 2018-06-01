64c09f65e9689bd7eb2a6447655c77e76c53ed71
Tim Harper <tharper@mesosphere.com>
2018-06-01 15:55:03 -0600

    Make boolean options configurable by specifying MARATHON_DISABLE_{...}=""

--- packages/marathon/extra/marathon.sh
+++ packages/marathon/extra/marathon.sh
@@ -13,11 +13,21 @@ export LIBPROCESS_IP=$($MESOS_IP_DISCOVERY_COMMAND)
 : ${MARATHON_MESOS_AUTHENTICATION_PRINCIPAL="dcos_marathon"}
 : ${MARATHON_MESOS_USER="root"}
 
+if [ -z "${MARATHON_DISABLE_ZK_COMPRESSION+x}" ]; then
+  MARATHON_ZK_COMPRESSION=""
+fi
+
+if [ -z "${MARATHON_DISABLE_REVIVE_OFFERS_FOR_NEW_APPS+x}" ]; then
+  MARATHON_REVIVE_OFFERS_FOR_NEW_APPS=""
+fi
+
+
 export JAVA_OPTS="${MARATHON_JAVA_ARGS-}"
 export -n MARATHON_JAVA_ARGS
 export MARATHON_HOSTNAME MARATHON_MESOS_ROLE MARATHON_MAX_INSTANCES_PER_OFFER \
        MARATHON_TASK_LAUNCH_TIMEOUT MARATHON_DECLINE_OFFER_DURATION MARATHON_ENABLE_FEATURES \
-       MARATHON_MESOS_AUTHENTICATION_PRINCIPAL MARATHON_MESOS_USER
+       MARATHON_MESOS_AUTHENTICATION_PRINCIPAL MARATHON_MESOS_USER MARATHON_ZK_COMPRESSION \
+       MARATHON_REVIVE_OFFERS_FOR_NEW_APPS
 
 # TODO (DCOS_OSS-3592) - move this variable to the exported list after MARATHON-8254 is fixed
 export -n MARATHON_DEFAULT_ACCEPTED_RESOURCE_ROLES
@@ -29,8 +39,6 @@ exec $PKG_PATH/marathon/bin/marathon \
     -J-XX:+PrintGCDetails \
     -J-XX:+PrintGCTimeStamps \
     --master zk://zk-1.zk:2181,zk-2.zk:2181,zk-3.zk:2181,zk-4.zk:2181,zk-5.zk:2181/mesos \
-    --revive_offers_for_new_apps \
-    --zk_compression \
     --mesos_leader_ui_url "/mesos" \
     # TODO (DCOS_OSS-3592) - remove this line after MARATHON-8254 is fixed
     --default_accepted_resource_roles "${MARATHON_DEFAULT_ACCEPTED_RESOURCE_ROLES}"
