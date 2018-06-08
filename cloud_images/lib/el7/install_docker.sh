67304526f502e663b518fdd472fda190b99ade2a
Senthil Kumaran <skumaran@mesosphere.com>
2018-06-08 06:39:44 -0700

    updated docker version.

--- cloud_images/lib/el7/install_docker.sh
+++ cloud_images/lib/el7/install_docker.sh
@@ -3,13 +3,9 @@ set -o errexit -o nounset -o pipefail
 
 echo ">>> In install_docker.sh:"
 
-yum -t -y remove container-selinux
-
 echo ">>> Installing Docker CE"
 curl -fLsSv --retry 20 -Y 100000 -y 60 -o /tmp/docker-ce-17.06.2.ce-1.el7.centos.x86_64.rpm \
   https://download.docker.com/linux/centos/7/x86_64/edge/Packages/docker-ce-17.06.2.ce-1.el7.centos.x86_64.rpm
-curl -fLsSv --retry 20 -Y 100000 -y 60 -o /tmp/docker-ce-selinux-17.05.0.ce-1.el7.centos.noarch.rpm \
-  https://download.docker.com/linux/centos/7/x86_64/edge/Packages/docker-ce-selinux-17.05.0.ce-1.el7.centos.noarch.rpm
 
 yum -t -y install /tmp/docker*.rpm || true
 systemctl enable docker
