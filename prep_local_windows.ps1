77928c55dfa9588d9b61a661c91ae542e1ffb104
Paul Allen <paulall@microsoft.com>
2018-08-10 16:45:49 -0700

    Updates to Windows local build scripts
    Mainly just cleanup
    Add extra flag to release call to generate bootstrap node

--- prep_local_windows.ps1
+++ prep_local_windows.ps1
@@ -4,13 +4,23 @@
 #
 # Expects that it is being run inside of a virtual environment.
 #
+$ErrorActionPreference = "Stop"
 
 if (!$env:VIRTUAL_ENV) {
-   throw "Must be run in a python virtual environment"
+   Write-Output "ERROR: Must be run in a python virtual environment. env:VIRTUAL_ENV is not set"
+   exit 1
 }
 
 # Install the latest version of pip
-pip install -U pip
+python -m pip install -U pip
+if( $LASTEXITCODE -ne 0 ) {
+    Write-Output "ERROR: Failed to upgrade pip"
+    exit 1
+}
 
 # Install the packages in editable mode to the virtual environment.
 pip install -e $PWD
+if( $LASTEXITCODE -ne 0 ) {
+    Write-Output "ERROR: Failed to install the packages into the virtual environment"
+    exit 1
+}
