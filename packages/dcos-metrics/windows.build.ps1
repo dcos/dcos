7313f189bba7f94f2dc22b6bcb9b9ecb66bfd7e7
Paul Allen <paulall@microsoft.com>
2018-08-10 14:54:39 -0700

    Build dcos-metrics for Windows agents
    Also includes a systemd unit file for installing and starting on Windows

--- packages/dcos-metrics/windows.build.ps1
+++ packages/dcos-metrics/windows.build.ps1
@@ -1,9 +1,31 @@
-$SRC_DIR = "c:\gopath\src\github.com\dcos\dcos-metrics\"
-new-item -itemtype directory "c:\gopath\src\github.com\dcos"
-copy-item -recurse  "c:\pkg\src\dcos-metrics" -destination "c:\gopath\src\github.com\dcos\"
-Push-Location $SRC_DIR
-& .\scripts\build.ps1 "collector" "statsd-emitter" "plugins"
-new-item -itemtype directory "$env:PKG_PATH/bin"
-Copy-Item -Path "$SRC_DIR\build\collector\dcos-metrics-collector-*" -Destination "$env:PKG_PATH/bin/dcos-metrics.exe"
-Copy-Item -Path "$SRC_DIR\build\statsd-emitter\dcos-metrics-statsd-emitter-*" -Destination "$env:PKG_PATH/bin/statsd-emitter.exe"
+$ErrorActionPreference = "Stop"
+
+$MESOSPHERE_DIR = Join-Path $env:SystemDrive "opt\mesosphere"
+$PKG_DIR = Join-Path $env:SystemDrive "pkg"
+
+
+. "$MESOSPHERE_DIR\environment.export.ps1"
+
+New-Item -ItemType "Directory" -Force -Path "$env:GOPATH\src\github.com\dcos"
+Copy-item -Recurse -Force "$PKG_DIR\src\dcos-metrics" -Destination "$env:GOPATH\src\github.com\dcos"
+Push-Location "$env:GOPATH\src\github.com\dcos\dcos-metrics"
+New-Item -ItemType "Directory" -Path ".\build"
+$p = Start-Process -Wait -PassThru -FilePath "powershell.exe" -ArgumentList @(".\scripts\build.ps1", "collector", "statsd-emitter", "plugins")
+if($p.ExitCode -ne 0) {
+    Throw "Failed to build dcos-metrics"
+}
+New-Item -ItemType "Directory" -Force -Path "$env:PKG_PATH\bin"
+Copy-Item -Path "$env:GOPATH\src\github.com\dcos\dcos-metrics\build\collector\dcos-metrics-collector-*" `
+          -Destination "$env:PKG_PATH\bin\dcos-metrics.exe"
+Copy-Item -Path "$env:GOPATH\src\github.com\dcos\dcos-metrics\build\statsd-emitter\dcos-metrics-statsd-emitter-*" `
+          -Destination "$env:PKG_PATH\bin\statsd-emitter.exe"
 Pop-Location
+
+$agentServiceDir = Join-Path $env:PKG_PATH "dcos.target.wants_slave"
+$agentPublicServiceDir = Join-Path $env:PKG_PATH "dcos.target.wants_slave_public"
+
+New-Item -ItemType "Directory" -Force -Path $agentServiceDir
+New-Item -ItemType "Directory" -Force -Path $agentPublicServiceDir
+
+Copy-Item -Path "$PKG_DIR\extra\dcos-metrics-agent.windows.service" -Destination "$agentServiceDir\dcos-metrics-agent.service"
+Copy-Item -Path "$PKG_DIR\extra\dcos-metrics-agent.windows.service" -Destination "$agentPublicServiceDir\dcos-metrics-agent.service"
