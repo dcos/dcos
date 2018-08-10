e892bf2b0976e0c153fea4495b7413660bdcfaaa
Paul Allen <paulall@microsoft.com>
2018-08-10 14:42:57 -0700

    Update adminrouter for Windows
    * Add dependency on the windows-apache Pkgpanda package
    * Add a Windows systemd service unit for running adminrouter on Windows

--- packages/adminrouter/windows.build.ps1
+++ packages/adminrouter/windows.build.ps1
@@ -1,3 +1,13 @@
-$ErrorActionPreference = "stop"
-New-Item -ItemType Directory "$env:PKG_PATH/bin/Apache24/conf"
-Copy-Item "pkg/extra/apache-windows/adminrouter.conf" "$env:PKG_PATH/bin/Apache24/conf"
+$ErrorActionPreference = "Stop"
+
+$MESOSPHERE_DIR = Join-Path $env:SystemDrive "opt\mesosphere"
+$PKG_DIR = Join-Path $env:SystemDrive "pkg"
+
+
+. "$MESOSPHERE_DIR\environment.export.ps1"
+
+New-Item -ItemType "Directory" -Force -Path "$env:PKG_PATH\bin\Apache24\conf"
+Copy-Item "$PKG_DIR\extra\apache-windows\adminrouter.conf" "$env:PKG_PATH\bin\Apache24\conf"
+
+New-Item -ItemType "Directory" -Force -Path "$env:PKG_PATH\dcos.target.wants"
+Copy-Item "$PKG_DIR\extra\systemd\dcos-adminrouter-agent.windows.service" "$env:PKG_PATH\dcos.target.wants\dcos-adminrouter-agent.service"
