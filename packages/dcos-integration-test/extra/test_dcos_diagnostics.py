fb661948503883b662fe72af339605af0637f0ee
Tomasz Janiszewski <janiszt@gmail.com>
2018-08-14 15:14:03 +0200

    Add mountnfo to diagnositc bundle
    This captures the host mount table allowing us to triage mount related issues. Note that getting the mount table for the dcos-diagnosis service (i.e., /proc/self) is OK because we don't put the service into a separate mount namespace: see: dcos-diagnostics-agent.service.
    
    See: http://man7.org/linux/man-pages/man5/proc.5.html
    
    Fixes: DCOS_OSS-3961

--- packages/dcos-integration-test/extra/test_dcos_diagnostics.py
+++ packages/dcos-integration-test/extra/test_dcos_diagnostics.py
@@ -512,7 +512,7 @@ def _download_bundle_from_master(dcos_api_session, master_index):
                              'opt/mesosphere/etc/dcos-version.json.gz', 'opt/mesosphere/etc/expanded.config.json.gz',
                              'opt/mesosphere/etc/user.config.yaml.gz', 'dcos-diagnostics-health.json',
                              'var/lib/dcos/cluster-id.gz', 'ps_aux_ww-4.output.gz',
-                             'proc/cmdline.gz', 'proc/cpuinfo.gz', 'proc/meminfo.gz']
+                             'proc/cmdline.gz', 'proc/cpuinfo.gz', 'proc/meminfo.gz', 'proc/self/mountinfo.gz']
 
     # these files are expected to be in archive for a master host
     expected_master_files = ['dcos-mesos-master.service.gz', 'var/lib/dcos/exhibitor/zookeeper/snapshot/myid.gz',
