aa77e28164be3963653fc356266cd4f9ea3f69a3
Paul Allen <paulall@microsoft.com>
2018-09-20 13:39:22 -0700

    Added constants to pkgpanda constants.py to be used in later commits.

--- pkgpanda/constants.py
+++ pkgpanda/constants.py
@@ -32,6 +32,8 @@ PKG_DIR = os.path.abspath("/pkg")
 config_dir = os.path.abspath('/etc/mesosphere')
 install_root = os.path.abspath('/opt/mesosphere')
 repository_base = install_root + os.sep + 'packages'
+systemd_system_root = os.path.abspath('/etc/systemd/system')
+profile_dir = os.path.abspath('/etc/profile.d')
 
 DCOS_SERVICE_CONFIGURATION_FILE = "dcos-service-configuration.json"
 DCOS_SERVICE_CONFIGURATION_PATH = install_root + os.sep + "etc" + os.sep + DCOS_SERVICE_CONFIGURATION_FILE
