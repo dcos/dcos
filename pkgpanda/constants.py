a1e5ccfa41941ea82b42682271da9951562658df
Paul Allen <paulall@microsoft.com>
2018-09-20 13:36:58 -0700

    Update file paths in constants.py to use abspath() where possible.
    Since abspath() will convert unix style paths to Windows style paths
    automatically, this makes the code easier to maintain in cases where the
    files exist in the same location on both systems.

--- pkgpanda/constants.py
+++ pkgpanda/constants.py
@@ -1,3 +1,5 @@
+import os
+
 from pkgpanda.util import is_windows
 
 RESERVED_UNIT_NAMES = [
@@ -9,13 +11,7 @@ RESERVED_UNIT_NAMES = [
 if is_windows:
     # windows specific directory locations
     # Note that these are not yet final and are placeholders
-    STATE_DIR_ROOT = 'c:\\var\\lib\\dcos'
-    PKG_DIR = "c:\\pkg"
-    config_dir = 'c:\\etc\\mesosphere'
-    install_root = 'c:\\opt\\mesosphere'
-    repository_base = install_root + '\\packages'
-
-    DOCKERFILE_DIR = 'docker.windows\\'
+    DOCKERFILE_DIR = 'docker.windows' + os.sep
 
     # Windows specific configuration files
     # System configuration on windows is quite different
@@ -24,18 +20,19 @@ if is_windows:
     dcos_services_yaml = 'dcos-services-windows.yaml'
     cloud_config_yaml = 'cloud-config-windows.yaml'
 else:
-    STATE_DIR_ROOT = '/var/lib/dcos'
-    PKG_DIR = "/pkg"
-    DOCKERFILE_DIR = 'docker/'
-    config_dir = '/etc/mesosphere'
-    install_root = '/opt/mesosphere'
-    repository_base = install_root + '/packages'
+    DOCKERFILE_DIR = 'docker' + os.sep
 
     # Non-windows specific configuration files.
     dcos_config_yaml = 'dcos-config.yaml'
     dcos_services_yaml = 'dcos-services.yaml'
     cloud_config_yaml = 'cloud-config.yaml'
 
+STATE_DIR_ROOT = os.path.abspath('/var/lib/dcos')
+PKG_DIR = os.path.abspath("/pkg")
+config_dir = os.path.abspath('/etc/mesosphere')
+install_root = os.path.abspath('/opt/mesosphere')
+repository_base = install_root + os.sep + 'packages'
+
 DCOS_SERVICE_CONFIGURATION_FILE = "dcos-service-configuration.json"
-DCOS_SERVICE_CONFIGURATION_PATH = install_root + "/etc/" + DCOS_SERVICE_CONFIGURATION_FILE
+DCOS_SERVICE_CONFIGURATION_PATH = install_root + os.sep + "etc" + os.sep + DCOS_SERVICE_CONFIGURATION_FILE
 SYSCTL_SETTING_KEY = "sysctl"
