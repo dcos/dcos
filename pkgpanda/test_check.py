fe8904bed0be143997ff1741c6746e08f3a158d7
Paul Allen <paulall@microsoft.com>
2018-10-18 04:20:25 -0400

    Updated pkgpanda 'test_check.py' to run tests on Windows.
    To do this, we copied a '.sh' script from the linux variant under
    pkgpanda/test_resources/unix/opt/mesosphere/packages
    to
    pkgpanda/test_resources/windows/opt/mesosphere/packages
    and changed it to a '.ps1' script
    
    We also changes a 'not_executable.py' file to a 'not_supported.dummy'
    file for windows.
    
    This simulates what the actual installed package on a DC/OS node will
    look like at the time a windows variant of that package is installed on
    a windows node.
    
    Since the check code for windows *only* works for '.py' and '.ps1' files
    (see code in pkgpanda/cli.py which manually invokes the proper
    interpreter for these file types), we will need to keep these tests in
    sync with any additional file types allow in the CLI code in the future.

--- pkgpanda/test_check.py
+++ pkgpanda/test_check.py
@@ -1,16 +1,23 @@
-from subprocess import check_output, PIPE, Popen, STDOUT
-
-import pytest
+from subprocess import PIPE, Popen
 
 from pkgpanda.util import is_windows, resources_test_dir
 
-list_output = """WARNING: `not_executable.py` is not executable
-pkg1--12345
+
+package_dir = "packages"
+
+if is_windows:
+    package_script = "shell_script_check.ps1"
+    output_stderr = "WARNING: `not_supported.dummy` file-type is not supported\n"
+else:
+    package_script = "shell_script_check.sh"
+    output_stderr = "WARNING: `not_executable.py` is not executable\n"
+
+list_output_stdout = """pkg1--12345
  - hello_world_ok.py
 pkg2--12345
  - failed_check.py
- - shell_script_check.sh
-"""
+ - {}
+""".format(package_script)
 
 run_output_stdout = """Hello World
 I exist to fail...
@@ -18,31 +25,29 @@ Assertion error
 Hello World
 """
 
-run_output_stderr = """WARNING: `not_executable.py` is not executable
-"""
 
-
-# TODO: DCOS_OSS-3469 - muted Windows tests requiring investigation
-@pytest.mark.skipif(is_windows, reason="test fails on Windows reason unknown")
 def test_check_target_list():
-    output = check_output([
+    cmd = Popen([
         'pkgpanda',
         'check',
         '--list',
         '--root', resources_test_dir("opt/mesosphere"),
-        '--repository', resources_test_dir("opt/mesosphere/packages")], stderr=STDOUT)
-    assert output.decode() == list_output
+        '--repository', resources_test_dir("opt/mesosphere/{}".format(package_dir))],
+        universal_newlines=True,
+        stdout=PIPE, stderr=PIPE)
+    stdout, stderr = cmd.communicate()
+    assert stdout == list_output_stdout
+    assert stderr == output_stderr
 
 
-# TODO: DCOS_OSS-3469 - muted Windows tests requiring investigation
-@pytest.mark.skipif(is_windows, reason="test fails on Windows reason unknown")
 def test_check_target_run():
     cmd = Popen([
         'pkgpanda',
         'check',
         '--root', resources_test_dir('opt/mesosphere'),
-        '--repository', resources_test_dir('opt/mesosphere/packages')],
+        '--repository', resources_test_dir('opt/mesosphere/{}'.format(package_dir))],
+        universal_newlines=True,
         stdout=PIPE, stderr=PIPE)
     stdout, stderr = cmd.communicate()
-    assert stdout.decode() == run_output_stdout
-    assert stderr.decode() == run_output_stderr
+    assert stdout == run_output_stdout
+    assert stderr == output_stderr
