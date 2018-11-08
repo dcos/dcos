cc251d281855ecc3c715e58932edcaf812003d8a
Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2018-11-08 22:51:41 +0100

    test_users: adjust, add comments

--- packages/dcos-integration-test/extra/test_users.py
+++ packages/dcos-integration-test/extra/test_users.py
@@ -74,21 +74,54 @@ def test_users_get(dcos_api_session):
             assert k in userdict
 
 
-def test_user_put_no_email_uid(dcos_api_session):
+def test_user_put_no_email_uid_empty_body(dcos_api_session):
+    # This test mainly demonstrates a subtle API difference between dcos-oauth
+    # (legacy) and Bouncer.
     r = dcos_api_session.put('/acs/api/v1/users/user1')
 
-    # This is the current behavior. It does not need to stay in future versions.
-    assert r.status_code == 500
-    assert 'invalid email' in r.text
+    # This is the old behavior in dcos-oauth.
+    # assert r.status_code == 500
+    # assert 'invalid email' in r.text
+
+    # With Bouncer non-email uids are valid, and the request fails as of the
+    # missing request body.
+    assert r.status_code == 400
+    assert 'Request has bad Content-Type or lacks JSON data' in r.text
+
+
+def test_legacy_user_creation_with_empty_json_doc(self):
+    # Legacy HTTP clients built for dcos-oauth such as the web UI (up to DC/OS
+    # 1.12) might insert users in the following way: uid appears to be an email
+    # address, and the JSON document in the request body does not provide a
+    # `public_key` or a `password` property (indicating local host), or is
+    # empty. The web UI expects those users to be remote users, usable with the
+    # legacy OIDC ID Token login method through the 'https://dcos.auth0.com/'
+    # provider. This behavior is maintained in Bouncer for backwards
+    # compatibility.
+    r = requests.put('/acs/api/v1/users/user@example.com', json={})
+    assert r.status_code == 201, r.text
+
+    # Bouncer annotates the created user (this is new compared to dcos-oauth).
+    r = requests.get('/acs/api/v1/users/users/user@example.com')
+    assert r.json()['provider_type'] == 'oidc'
+    assert r.json()['provider_id'] == 'https://dcos.auth0.com/'
+    assert r.json()['is_remote'] == True
+
+    # When the uid however does not appear to be an email address the more sane
+    # behavior of Bouncer takes effect: an empty (meaningless) JSON body
+    # results in a useful error message.
+    r = requests.put('/acs/api/v1/users/user1', json={})
+    assert r.status_code == 400
+    assert 'One of `password` or `public_key` must be provided' in r.text
 
 
 @pytest.mark.usefixtures('remove_users_added_by_test')
-def test_user_put_email_uid(dcos_api_session):
-    # The current behavior is that the request body can be an empty JSON
-    # document. It does not need to stay in future versions.
-    r = dcos_api_session.put('/acs/api/v1/users/user1@email.de', json={})
-    r.raise_for_status()
-    assert r.status_code == 201
+def test_user_put_email_uid_and_description(dcos_api_session):
+    r = dcos_api_session.put(
+        '/acs/api/v1/users/user1@email.de',
+        json={'description': 'integration test user'}
+    )
+    assert r.status_code == 201, r.text
 
     users = get_users(dcos_api_session)
     assert len(users) > 1
@@ -96,42 +129,27 @@ def test_user_put_email_uid(dcos_api_session):
 
 
 @pytest.mark.usefixtures('remove_users_added_by_test')
-def test_user_put_optional_payload(dcos_api_session):
-    # This is the current behavior. It does not need to stay in future versions.
-
+def test_user_put_with_legacy_body(dcos_api_session):
+    # The UI up to DC/OS 1.12 sends the `creator_uid` and the `cluster_url`
+    # properties although they are not used by dcos-oauth. Bouncer supports
+    # these two properties for legacy reasons. Note(JP): As a follow-up task we
+    # should change the UI to not send these properties anymore, and then remove
+    # the properties from Bouncer's UserCreate JSON schema again, ideally within
+    # the 1.13 development cycle.
     r = dcos_api_session.put(
         '/acs/api/v1/users/user2@email.de',
         json={'creator_uid': 'any@thing.bla', 'cluster_url': 'foobar'}
-    )
-    assert r.status_code == 201, r.text
-
-    r = dcos_api_session.put(
-        '/acs/api/v1/users/user3@email.de',
-        json={'creator_uid': 'any@thing.bla', 'description': 'barfoo'}
-    )
-    assert r.status_code == 201, r.text
-
-    r = dcos_api_session.put(
-        '/acs/api/v1/users/user4@email.de',
-        json={'is_remote': False}
     )
     assert r.status_code == 201, r.text
 
 
 @pytest.mark.usefixtures('remove_users_added_by_test')
 def test_user_conflict(dcos_api_session):
-    # This is the current behavior. It does not need to stay in future versions.
-
-    r = dcos_api_session.put(
-        '/acs/api/v1/users/user2@email.de',
-        json={'creator_uid': 'any@thing.bla', 'cluster_url': 'foobar'}
-    )
+    # Note: the empty request body is not the decisive criterion here.
+    r = dcos_api_session.put('/acs/api/v1/users/user2@email.de', json={})
     assert r.status_code == 201, r.text
 
-    r = dcos_api_session.put(
-        '/acs/api/v1/users/user2@email.de',
-        json={'creator_uid': 'any@thing.bla', 'cluster_url': 'foobar'}
-    )
+    r = dcos_api_session.put('/acs/api/v1/users/user2@email.de', json={})
     assert r.status_code == 409, r.text
 
 
