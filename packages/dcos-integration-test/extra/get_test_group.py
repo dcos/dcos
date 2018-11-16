2aa726b306424552bf9c6aa223de30512a955520
Adam Dangoor <adamdangoor@gmail.com>
2018-11-16 14:53:45 +0000

    Add a file which separates tests into groups
    (cherry picked from commit 05bb8f040383863cf5f799255c97bcea0aa5de8b)

---
+++ packages/dcos-integration-test/extra/get_test_group.py
@@ -0,0 +1,41 @@
+"""
+Usage:
+
+$ python test_group_name.py group_1
+test_foo.py test_bar.py::TestClass
+$
+
+This is used by CI to run only a certain set of tests on a particular builder.
+
+See ``test_groups.yaml`` for details.
+"""
+
+import yaml
+from pathlib import Path
+from typing import List
+
+import click
+
+
+def patterns_from_group(group_name: str) -> List[str]:
+    """
+    Given a group name, return all the pytest patterns defined for that group
+    in ``test_groups.yaml``.
+    """
+    test_group_file = Path(__file__).parent / 'test_groups.yaml'
+    test_group_file_contents = test_group_file.read_text()
+    test_groups = yaml.load(test_group_file_contents)['groups']
+    return test_groups[group_name]
+
+@click.command('list-integration-test-patterns')
+@click.argument('group_name')
+def list_integration_test_patterns(group_name: str) -> None:
+    """
+    Perform a release.
+    """
+    test_patterns = patterns_from_group(group_name=group_name)
+    click.echo(' '.join(test_patterns))
+
+
+if __name__ == '__main__':
+    list_integration_test_patterns()
---
+++ packages/dcos-integration-test/extra/test_meta.py
@@ -0,0 +1,61 @@
+"""
+Tests for the integration test suite itself.
+"""
+
+import subprocess
+from typing import Set
+
+import yaml
+
+from get_test_group import patterns_from_group
+
+
+def _tests_from_pattern(ci_pattern: str) -> Set[str]:
+    """
+    From a CI pattern, get all tests ``pytest`` would collect.
+    """
+    tests = set([])  # type: Set[str]
+    args = ['pytest', '--collect-only', ci_pattern, '-q']
+    result = subprocess.run(args=args, stdout=subprocess.PIPE)
+    output = result.stdout
+    for line in output.splitlines():
+        if line and not line.startswith(b'no tests ran in'):
+            tests.add(line.decode())
+
+    return tests
+
+
+def test_test_groups() -> None:
+    """
+    The test suite is split into various "groups".
+    This test confirms that the groups together contain all tests, and each
+    test is collected only once.
+    """
+    groups = {'group_1', 'group_2', 'group_3', 'group_4'}
+    test_patterns = []
+    for group in groups:
+        test_patterns += patterns_from_group(group_name=group)
+
+    tests_to_patterns = {}  # type: Dict[str, Set[str]]
+    for pattern in test_patterns:
+        tests = _tests_from_pattern(ci_pattern=pattern)
+        for test in tests:
+            if test in tests_to_patterns:
+                tests_to_patterns[test].add(pattern)
+            else:
+                tests_to_patterns[test] = set([pattern])
+
+    for test_name, patterns in tests_to_patterns.items():
+        message = (
+            'Test "{test_name}" will be run once for each pattern in '
+            '{patterns}. '
+            'Each test should be run only once.'
+        ).format(
+            test_name=test_name,
+            patterns=patterns,
+        )
+        assert len(patterns) == 1, message
+
+    all_tests = _tests_from_pattern(ci_pattern='')
+    assert tests_to_patterns.keys() - all_tests == set()
+    assert all_tests - tests_to_patterns.keys() == set()
