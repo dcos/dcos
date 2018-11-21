bf709881104a782742f4f2e80a2583ad1c95e77a
Grace Do <xgrace@gmail.com>
2018-11-21 13:16:15 -0800

    Extract package install/teardown into context manager

--- packages/dcos-integration-test/extra/test_metrics.py
+++ packages/dcos-integration-test/extra/test_metrics.py
@@ -1,3 +1,5 @@
+import contextlib
+
 import common
 import pytest
 import retrying
@@ -46,7 +48,7 @@ def test_metrics_masters_prom(dcos_api_session):
         assert response.status_code == 200, 'Status code: {}'.format(response.status_code)
 
 
-@retrying.retry(wait_fixed=2000, stop_max_delay=300 * 1000)
+@retrying.retry(wait_fixed=5000, stop_max_delay=300 * 1000)
 def get_metrics_prom(dcos_api_session, node):
     """Gets metrics from prometheus port on node and returns the response.
 
@@ -148,15 +150,40 @@ def test_metrics_agents_statsd(dcos_api_session):
             assert len(endpoints) == 1, 'The marathon app should have been deployed exactly once.'
 
             @retrying.retry(wait_fixed=2000, stop_max_delay=300 * 1000)
-            def check_statsd_metrics(response):
+            def check_statsd_metrics():
                 response = get_metrics_prom(dcos_api_session, agent)
                 for metric_name in expected_metrics:
                     assert metric_name in response.text
             check_statsd_metrics()
 
 
+@contextlib.contextmanager
+def deploy_and_cleanup_dcos_package(dcos_api_session, package_name, package_version, framework_name):
+    """Deploys dcos package and waits for package teardown once the context is left"""
+    app_id = dcos_api_session.cosmos.install_package(package_name, package_version=package_version).json()['appId']
+    dcos_api_session.marathon.wait_for_deployments_complete()
+
+    try:
+        yield
+    finally:
+        dcos_api_session.cosmos.uninstall_package(package_name, app_id=app_id)
+
+        # Retry for 150 seconds for teardown completion
+        @retrying.retry(wait_fixed=5000, stop_max_delay=150 * 1000)
+        def wait_for_package_teardown():
+            state_response = dcos_api_session.get('/state', host=dcos_api_session.masters[0], port=5050)
+            assert state_response.status_code == 200
+            state = state_response.json()
+
+            for framework in state['frameworks']:
+                if framework['name'] == framework_name:
+                    raise Exception('Framework {} still running'.format(framework_name))
+        wait_for_package_teardown()
+
+
 @retrying.retry(wait_fixed=2000, stop_max_delay=300 * 1000)
 def get_task_hostname(dcos_api_session, framework_name, task_name):
+    # helper func that gets a framework's task's hostname
     mesos_id = node = ''
     state_response = dcos_api_session.get('/state', host=dcos_api_session.masters[0], port=5050)
     assert state_response.status_code == 200
@@ -182,42 +209,23 @@ def get_task_hostname(dcos_api_session, framework_name, task_name):
 
 def test_task_metrics_metadata(dcos_api_session):
     """Test that task metrics have expected metadata/labels"""
-    install_response = dcos_api_session.cosmos.install_package('marathon')
-    data = install_response.json()
-    dcos_api_session.marathon.wait_for_deployments_complete()
+    with deploy_and_cleanup_dcos_package(dcos_api_session, 'marathon', '1.6.535', 'marathon-user'):
+        node = get_task_hostname(dcos_api_session, 'marathon', 'marathon-user')
 
-    node = get_task_hostname(dcos_api_session, 'marathon', 'marathon-user')
-
-    @retrying.retry(wait_fixed=2000, stop_max_delay=300 * 1000)
-    def check_metrics_metadata(response):
-        response = get_metrics_prom(dcos_api_session, node)
-        for line in response.text.splitlines():
-            if '#' in line:
-                continue
-            if 'task_name="marathon-user"' in line:
-                assert 'service_name="marathon"' in line
-    check_metrics_metadata()
-
-    # uninstall and cleanup framework
-    dcos_api_session.cosmos.uninstall_package('marathon', app_id=data['appId'])
+        @retrying.retry(wait_fixed=2000, stop_max_delay=300 * 1000)
+        def check_metrics_metadata():
+            response = get_metrics_prom(dcos_api_session, node)
+            for line in response.text.splitlines():
+                if '#' in line:
+                    continue
+                if 'task_name="marathon-user"' in line:
+                    assert 'service_name="marathon"' in line
+        check_metrics_metadata()
 
 
 def test_executor_metrics_metadata(dcos_api_session):
     """Test that executor metrics have expected metadata/labels"""
-    # get count of all running containers to check against later for complete teardown
-    num_containers = 0
-    for agent_ip in dcos_api_session.slaves:
-        response = dcos_api_session.metrics.get('/containers', node=agent_ip)
-        assert response.status_code == 200
-        num_containers += len(response.json())
-
-    try:
-        # install hello-world framework
-        install_response = dcos_api_session.cosmos.install_package('hello-world', package_version='2.2.0-0.42.2')
-        data = install_response.json()
-
-        dcos_api_session.marathon.wait_for_deployments_complete()
-
+    with deploy_and_cleanup_dcos_package(dcos_api_session, 'hello-world', '2.2.0-0.42.2', 'hello-world'):
         node = get_task_hostname(dcos_api_session, 'marathon', 'hello-world')
 
         @retrying.retry(wait_fixed=2000, stop_max_delay=300 * 1000)
@@ -234,21 +242,6 @@ def test_executor_metrics_metadata(dcos_api_session):
                     # hello-world executors can be named "hello" or "world"
                     assert ('executor_name="hello"' in line or 'executor_name="world"' in line)
         check_executor_metrics_metadata()
-    finally:
-        # uninstall and cleanup framework
-        dcos_api_session.cosmos.uninstall_package('hello-world', app_id=data['appId'])
-
-        # Retry for 150 seconds for teardown completion
-        @retrying.retry(wait_fixed=2000, stop_max_delay=150 * 1000)
-        def wait_for_framework_teardown():
-            num_containers_check = 0
-            for agent_ip in dcos_api_session.slaves:
-                response = dcos_api_session.metrics.get('/containers', node=agent_ip)
-                assert response.status_code == 200
-                num_containers_check += len(response.json())
-            assert num_containers_check <= num_containers
-
-        wait_for_framework_teardown()
 
 
 @pytest.mark.supportedwindows
