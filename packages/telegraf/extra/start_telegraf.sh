fb10597cec1c1b224adba811232ea42e9fefd14c
Grace Do <xgrace@gmail.com>
2018-12-12 12:14:44 -0800

    Add symlinks to user-editable telegraf conf files in config directory

--- packages/telegraf/extra/start_telegraf.sh
+++ packages/telegraf/extra/start_telegraf.sh
@@ -24,6 +24,14 @@ mkdir -p "${TELEGRAF_CONTAINERS_DIR}"
 # Migrate old containers dir to new location in case the cluster was upgraded.
 /opt/mesosphere/active/telegraf/tools/migrate_containers_dir.sh "${LEGACY_CONTAINERS_DIR}" "${TELEGRAF_CONTAINERS_DIR}"
 
+# Add symlinks to any additional configuration files in the user-editable Telegraf config directory
+shopt -s nullglob
+for file in ${TELEGRAF_EXTRA_CONFIG_DIR}*; do
+	ln -sfn $file ${TELEGRAF_CONFIG_DIR}$(basename "$file")
+done
+# Remove any broken symlinks
+find ${TELEGRAF_CONFIG_DIR} -type l ! -exec test -e {} \; -exec rm {} \;
+
 # Ensure that old socket file is removed, if present
 # TODO(philipnrmn): investigate whether moving to a systemd-managed socket
 # would be a better solution than manually creating and removing this file.
