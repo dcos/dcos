5c256537a0a39120061d098d30a3984afdaa7155
Rodney Norris <rnorris@mesosphere.com>
2019-02-01 10:02:22 -0600

    chore(uiupdateservice): use state dir
    Updated to use the `/var/lib/dcos/ui-update-service` as a state
    directory instead of storing version & symlinks in the package directory
    which is removed between dcos updates.
    
    Updated to create and use
    `/opt/mesosphere/etc/dcos-ui-update-service.env` for CLI args for the
    service and its pre-start script.

--- packages/dcos-ui-update-service/extra/dcos-ui-update-service-pre-start.sh
+++ packages/dcos-ui-update-service/extra/dcos-ui-update-service-pre-start.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
+# Ensure env variables are loaded
+set -a
+source /opt/mesosphere/etc/dcos-ui-update-service.env
+set +a
+
 # Create versions dir and set permissions correctly
-mkdir -p /opt/mesosphere/active/dcos-ui-update-service/versions
-chmod 775 /opt/mesosphere/active/dcos-ui-update-service/versions
-chown root:dcos_ui_update_service /opt/mesosphere/active/dcos-ui-update-service/versions
+mkdir -p $DCOS_UI_UPDATE_VERSIONS_ROOT
+chmod 775 $DCOS_UI_UPDATE_VERSIONS_ROOT
 
 # Create dist folder for symlinks
-mkdir -p /opt/mesosphere/active/dcos-ui-update-service/dist
-chmod 775 /opt/mesosphere/active/dcos-ui-update-service/dist
-chown root:dcos_ui_update_service /opt/mesosphere/active/dcos-ui-update-service/dist
+mkdir -p $DCOS_UI_UPDATE_DIST_PATH
+chmod 775 $DCOS_UI_UPDATE_DIST_PATH
 
-if [ -f /opt/mesosphere/active/dcos-ui-update-service/dist/ui ]
-then
+chown -R root:dcos_ui_update_service $DCOS_UI_UPDATE_STATE_PATH
+
+if [ -f $DCOS_UI_DIST_LINK ] 
+then 
     echo "ui-update-service UI-Dist symlink already exists"
 else
     echo "ui-update-service UI-Dist symlink does not exist, creating it."
-    ln -s /opt/mesosphere/active/dcos-ui/usr /opt/mesosphere/active/dcos-ui-update-service/dist/ui
+    ln -s $DCOS_UI_UPDATE_DEFAULT_UI_PATH $DCOS_UI_UPDATE_DIST_LINK
 fi
-chown root:dcos_ui_update_service /opt/mesosphere/active/dcos-ui-update-service/dist/ui
+chown root:dcos_ui_update_service $DCOS_UI_UPDATE_DIST_LINK
