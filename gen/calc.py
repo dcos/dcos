68b0f7f6690dd70e22d219927dd7e386a0dd5814
Senthil Kumaran <senthil@uthcode.com>
2019-02-12 13:34:51 -0800

    update the version of DC/OS to 1.11.10

--- gen/calc.py
+++ gen/calc.py
@@ -1093,7 +1093,7 @@ entry = {
         'mesos_agent_log_file': '/var/log/mesos/mesos-agent.log',
         'mesos_master_log_file': '/var/lib/dcos/mesos/log/mesos-master.log',
         'marathon_port': '8080',
-        'dcos_version': '1.11.9.1',
+        'dcos_version': '1.11.10',
         'dcos_gen_resolvconf_search_str': calculate_gen_resolvconf_search,
         'curly_pound': '{#',
         'exhibitor_static_ensemble': calculate_exhibitor_static_ensemble,
