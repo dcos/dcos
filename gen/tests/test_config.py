fba3d857460b8ad62f7d3e0b4575bbb3b39d614f
Branden Rolston <branden@mesosphere.com>
2019-02-25 20:28:30 -0800

    Fix tests for schema 0.7.0
    The 0.7.0 release of the schema package changed the wording in some
    error messages. This updates tests to reflect the change.

--- gen/tests/test_config.py
+++ gen/tests/test_config.py
@@ -289,12 +289,12 @@ def test_validate_check_config():
     validate_error(
         {'check_config': json.dumps({'cluster_checks': {}})},
         'check_config',
-        "Key 'cluster_checks' error: Missing keys: Check name must be a nonzero length string with no whitespace",
+        "Key 'cluster_checks' error: Missing key: Check name must be a nonzero length string with no whitespace",
     )
     validate_error(
         {'check_config': json.dumps({'node_checks': {}})},
         'check_config',
-        "Key 'node_checks' error: Missing keys: 'checks'",
+        "Key 'node_checks' error: Missing key: 'checks'",
     )
     validate_error(
         {
@@ -306,7 +306,7 @@ def test_validate_check_config():
         },
         'check_config',
         (
-            "Key 'node_checks' error: Key 'checks' error: Missing keys: Check name must be a nonzero length string "
+            "Key 'node_checks' error: Key 'checks' error: Missing key: Check name must be a nonzero length string "
             "with no whitespace"
         ),
     )
@@ -325,7 +325,7 @@ def test_validate_check_config():
             })
         },
         'check_config',
-        "Key 'cluster_checks' error: Missing keys: Check name must be a nonzero length string with no whitespace",
+        "Key 'cluster_checks' error: Missing key: Check name must be a nonzero length string with no whitespace",
     )
     validate_error(
         {
@@ -344,7 +344,7 @@ def test_validate_check_config():
         },
         'check_config',
         (
-            "Key 'node_checks' error: Key 'checks' error: Missing keys: Check name must be a nonzero length string "
+            "Key 'node_checks' error: Key 'checks' error: Missing key: Check name must be a nonzero length string "
             "with no whitespace"
         ),
     )
@@ -433,7 +433,7 @@ def test_validate_check_config():
             })
         },
         'check_config',
-        "Key 'cluster_checks' error: Key 'cluster-check-1' error: Missing keys: 'description'",
+        "Key 'cluster_checks' error: Key 'cluster-check-1' error: Missing key: 'description'",
     )
     validate_error(
         {
@@ -450,7 +450,7 @@ def test_validate_check_config():
             })
         },
         'check_config',
-        "Key 'node_checks' error: Key 'checks' error: Key 'node-check-1' error: Missing keys: 'description'",
+        "Key 'node_checks' error: Key 'checks' error: Key 'node-check-1' error: Missing key: 'description'",
     )
 
     # Check cmd is wrong type.
