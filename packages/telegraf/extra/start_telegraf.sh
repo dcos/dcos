30dc54f4870f13272f5d99703a86385fa6c2c647
Branden Rolston <branden@mesosphere.com>
2019-02-26 19:49:53 -0800

    Manage Telegraf's dcos_statsd socket with systemd

--- packages/telegraf/extra/start_telegraf.sh
+++ packages/telegraf/extra/start_telegraf.sh
@@ -35,9 +35,5 @@ mkdir -p "${TELEGRAF_CONTAINERS_DIR}"
 # Migrate old containers dir to new location in case the cluster was upgraded.
 /opt/mesosphere/active/telegraf/tools/migrate_containers_dir.sh "${LEGACY_CONTAINERS_DIR}" "${TELEGRAF_CONTAINERS_DIR}"
 
-# Ensure that old socket file is removed, if present
-# TODO(philipnrmn): investigate whether moving to a systemd-managed socket
-# would be a better solution than manually creating and removing this file.
-rm -f /run/dcos/telegraf/dcos_statsd.sock
 # Start telegraf.
 exec /opt/mesosphere/bin/telegraf "$@"
