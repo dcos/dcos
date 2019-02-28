bc13c150967d799046bb40513c216024c8e3dd18
Tim Weidner <tweidner@mesosphere.com>
2019-02-28 14:28:40 +0100

    Revert changes from Enterprise

--- test-e2e/test_master_node_replacement.py
+++ test-e2e/test_master_node_replacement.py
@@ -145,7 +145,7 @@ def test_replace_all_static(
         try:
             for master_to_be_replaced in original_masters:
                 # Destroy a master and free one IP address.
-                current_cluster.destroy_node(node=master_to_be_replaced)
+                original_cluster.destroy_node(node=master_to_be_replaced)
 
                 temporary_cluster = Cluster(
                     cluster_backend=docker_backend,
