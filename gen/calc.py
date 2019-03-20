896b96eee12b137c6fd4c9863d2fd8f9d7901653
Drew Flower <dflower@mesosphere.com>
2019-03-20 05:50:01 -0600

    Update ip_detect_script check to 30s
    The ip_detect_script check calls the detect_ip script on the system and
    validates the response. On AWS systems, this check makes a request to an
    external AWS metadata endpoint. Occassionally this request takes longer
    than 1s (current timeout).

--- gen/calc.py
+++ gen/calc.py
@@ -849,7 +849,7 @@ def calculate_check_config(check_time):
                 'ip_detect_script': {
                     'description': 'The IP detect script produces valid output',
                     'cmd': ['/opt/mesosphere/bin/dcos-checks', 'ip'],
-                    'timeout': instant_check_timeout
+                    'timeout': normal_check_timeout
                 },
                 'docker': {
                     'description': 'Docker is installed',
