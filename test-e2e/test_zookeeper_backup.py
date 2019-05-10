ce22a3ac0154c2653288811c83acb1ae5b9548eb
Tim Weidner <tweidner@mesosphere.com>
2019-05-10 17:40:20 +0200

    DCOS-53076 Add ZooKeeper backup/restore script and E2E tests
    This PR automates the backup/restore procedure for
    ZooKeeper instances on DC/OS master nodes. It provides
    us with a consistent way of doing backups. And allows
    customers to take a ZooKeeper backup more easily.
    
    This makes Guano for backing up ZooKeeper obsolete.

---
+++ test-e2e/test_zookeeper_backup.py
@@ -0,0 +1,278 @@
+"""
+Tests for the ZooKeeper backup/restore via `dcos-zk`.
+"""
+
+import logging
+import uuid
+from pathlib import Path
+from typing import Set
+
+from _pytest.fixtures import SubRequest
+
+from cluster_helpers import wait_for_dcos_oss
+from dcos_e2e.backends import Docker
+from dcos_e2e.cluster import Cluster
+from dcos_e2e.node import Node, Output
+from kazoo.client import KazooClient
+from kazoo.exceptions import NoNodeError
+
+
+LOGGER = logging.getLogger(__name__)
+
+# Arbitrary value written to ZooKeeper.
+FLAG = b'flag'
+
+
+def _zk_set_flag(hosts: str, ephemeral: bool = False) -> str:
+    """
+    Store the `FLAG` value in ZooKeeper in a random Znode.
+    """
+    znode = '/{}'.format(uuid.uuid4())
+    zk_client = KazooClient(hosts=hosts)
+    zk_client.start()
+    try:
+        zk_client.create(znode, makepath=True, ephemeral=ephemeral)
+        zk_client.set(znode, FLAG)
+    finally:
+        zk_client.stop()
+    return znode
+
+
+def _zk_flag_exists(hosts: str, znode: str) -> bool:
+    """
+    The `FLAG` value exists in ZooKeeper at the `znode` path.
+    """
+    zk_client = KazooClient(hosts=hosts, read_only=True)
+    zk_client.start()
+    try:
+        value = zk_client.get(znode)
+    except NoNodeError:
+        return False
+    finally:
+        zk_client.stop()
+    return bool(value[0] == FLAG)
+
+
+def _do_backup(master: Node, backup_local_path: Path) -> None:
+    """
+    Automated ZooKeeper backup procedure.
+    Intended to be consistent with the documentation.
+    https://jira.mesosphere.com/browse/DCOS-51647
+    """
+    master.run(args=['systemctl', 'stop', 'dcos-exhibitor'])
+
+    backup_name = backup_local_path.name
+    # Note that this must be an existing directory on the remote server.
+    backup_remote_path = Path('/etc/') / backup_name
+    master.run(
+        args=[
+            '/opt/mesosphere/bin/dcos-shell',
+            'dcos-zk',
+            'backup',
+            str(backup_remote_path),
+            '-v',
+        ],
+        output=Output.LOG_AND_CAPTURE,
+    )
+
+    master.run(args=['systemctl', 'start', 'dcos-exhibitor'])
+
+    master.download_file(
+        remote_path=backup_remote_path,
+        local_path=backup_local_path,
+    )
+
+    master.run(args=['rm', str(backup_remote_path)])
+
+
+def _do_restore(all_masters: Set[Node], backup_local_path: Path) -> None:
+    """
+    Automated ZooKeeper restore from backup procedure.
+    Intended to be consistent with the documentation.
+    https://jira.mesosphere.com/browse/DCOS-51647
+    """
+    backup_name = backup_local_path.name
+    backup_remote_path = Path('/etc/') / backup_name
+
+    for master in all_masters:
+        master.send_file(
+            local_path=backup_local_path,
+            remote_path=backup_remote_path,
+        )
+
+    for master in all_masters:
+        master.run(args=['systemctl', 'stop', 'dcos-exhibitor'])
+
+    for master in all_masters:
+        master.run(
+            args=[
+                '/opt/mesosphere/bin/dcos-shell',
+                'dcos-zk', 'restore', str(backup_remote_path), '-v',
+            ],
+            output=Output.LOG_AND_CAPTURE,
+        )
+
+    for master in all_masters:
+        master.run(args=['systemctl', 'start', 'dcos-exhibitor'])
+
+
+class TestZooKeeperBackup:
+    """
+    Within the context of DC/OS ZooKeeper can be backed up on a running
+    cluster and a previous state can be restored.
+    """
+
+    def test_transaction_log_backup_and_restore(
+        self,
+        artifact_path: Path,
+        docker_backend: Docker,
+        tmp_path: Path,
+        request: SubRequest,
+        log_dir: Path,
+    ) -> None:
+        """
+        In a 3-master cluster, backing up the transaction log of ZooKeeper on
+        one node and restoring from the backup on all master results in a
+        functioning DC/OS cluster with previously backed up Znodes restored.
+        """
+        with Cluster(
+            cluster_backend=docker_backend,
+            masters=3,
+            agents=0,
+            public_agents=0,
+        ) as cluster:
+            cluster.install_dcos_from_path(
+                dcos_installer=artifact_path,
+                dcos_config=cluster.base_config,
+                ip_detect_path=docker_backend.ip_detect_path,
+            )
+            wait_for_dcos_oss(
+                cluster=cluster,
+                request=request,
+                log_dir=log_dir,
+            )
+            master = next(iter(cluster.masters))
+            master_ips = [str(m.public_ip_address) for m in cluster.masters]
+            zk_hostports = ','.join([m + ':2181' for m in master_ips])
+
+            # Write to ZooKeeper before backup
+            persistent_flag = _zk_set_flag(zk_hostports)
+            ephemeral_flag = _zk_set_flag(zk_hostports, ephemeral=True)
+
+            random = uuid.uuid4().hex
+            backup_name = 'zk-backup-{random}.tar.gz'.format(random=random)
+            backup_local_path = tmp_path / backup_name
+
+            # Take ZooKeeper backup from one master node.
+            _do_backup(master, backup_local_path)
+
+            # Store a datapoint which we expect to be lost.
+            not_backed_up_flag = _zk_set_flag(zk_hostports)
+
+            # Restore ZooKeeper from backup on all master nodes.
+            _do_restore(cluster.masters, backup_local_path)
+
+            # Assert that DC/OS is intact.
+            wait_for_dcos_oss(
+                cluster=cluster,
+                request=request,
+                log_dir=log_dir,
+            )
+
+            # Read from ZooKeeper after restore
+            assert _zk_flag_exists(zk_hostports, persistent_flag)
+            assert not _zk_flag_exists(zk_hostports, ephemeral_flag)
+            assert not _zk_flag_exists(zk_hostports, not_backed_up_flag)
+
+    def test_snapshot_backup_and_restore(
+        self,
+        artifact_path: Path,
+        docker_backend: Docker,
+        tmp_path: Path,
+        request: SubRequest,
+        log_dir: Path,
+    ) -> None:
+        """
+        In a 3-master cluster, backing up a snapshot of ZooKeeper on
+        one node and restoring from the backup on all master results in a
+        functioning DC/OS cluster with previously backed up Znodes restored.
+        """
+        with Cluster(
+            cluster_backend=docker_backend,
+            masters=3,
+            agents=0,
+            public_agents=0,
+        ) as cluster:
+            cluster.install_dcos_from_path(
+                dcos_installer=artifact_path,
+                dcos_config=cluster.base_config,
+                ip_detect_path=docker_backend.ip_detect_path,
+            )
+            wait_for_dcos_oss(
+                cluster=cluster,
+                request=request,
+                log_dir=log_dir,
+            )
+            masters = cluster.masters
+
+            # Modify Exhibitor conf, generating ZooKeeper conf (set
+            # `snapCount=1`). This config change instructs ZooKeeper to Adding
+            # the `snapCount` here only works as long as DC/OS does not set it.
+            args = [
+                'sed',
+                '-i', "'s/zoo-cfg-extra=/zoo-cfg-extra=snapCount\\\\=1\\&/'",
+                '/opt/mesosphere/active/exhibitor/usr/exhibitor/start_exhibitor.py',
+            ]
+            for master in masters:
+                master.run(
+                    args=args,
+                    shell=True,
+                    output=Output.LOG_AND_CAPTURE,
+                )
+            for master in masters:
+                # Restart Exhibitor
+                master.run(['systemctl', 'restart', 'dcos-exhibitor'])
+
+            wait_for_dcos_oss(
+                cluster=cluster,
+                request=request,
+                log_dir=log_dir,
+            )
+
+            # Begin the backup/restore test procedure
+            master_ips = [str(m.public_ip_address) for m in masters]
+            zk_hostports = ','.join([m + ':2181' for m in master_ips])
+
+            # Write to ZooKeeper multiple times before backup
+            persistent_flag = _zk_set_flag(zk_hostports)
+            ephemeral_flag = _zk_set_flag(zk_hostports, ephemeral=True)
+
+            # Extra ZooKeeper write, triggering snapshot creation due to
+            # `snapCount=1`. After this we can be sure the previous writes are
+            # contained in at least one of the generated snapshots.
+            _zk_set_flag(zk_hostports)
+
+            random = uuid.uuid4().hex
+            backup_name = 'zk-backup-{random}.tar.gz'.format(random=random)
+            backup_local_path = tmp_path / backup_name
+
+            # Take ZooKeeper backup from one master node.
+            _do_backup(master, backup_local_path)
+
+            # Store a datapoint which we expect to be lost.
+            not_backed_up_flag = _zk_set_flag(zk_hostports)
+
+            # Restore ZooKeeper from backup on all master nodes.
+            _do_restore(cluster.masters, backup_local_path)
+
+            # Assert DC/OS is intact.
+            wait_for_dcos_oss(
+                cluster=cluster,
+                request=request,
+                log_dir=log_dir,
+            )
+
+            # Read from ZooKeeper after restore
+            assert _zk_flag_exists(zk_hostports, persistent_flag)
+            assert not _zk_flag_exists(zk_hostports, ephemeral_flag)
+            assert not _zk_flag_exists(zk_hostports, not_backed_up_flag)
