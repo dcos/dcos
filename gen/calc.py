99e0d75a6a5c024639ece7c3e241d0a6c6a7bf40
Brian Lewis <keller@csh.rit.edu>
2019-05-17 14:41:10 +0200

    Bump "instant" timeout to 5 seconds to hopefully account for shelling out on a slower or bogged down system

--- gen/calc.py
+++ gen/calc.py
@@ -824,7 +824,7 @@ def calculate_check_config(check_time):
     # The 30s value was chosen arbitrarily. It may be increased in the future as required.
     # We chose not to use a value greater than 1min, as the checks are automatically executed
     # in parallel every minute.
-    instant_check_timeout = "1s"
+    instant_check_timeout = "5s"
     normal_check_timeout = "30s"
     check_config = {
         'node_checks': {
