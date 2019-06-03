cb631bc5c965612278474b686c38e39199f8e053
Tomasz Janiszewski <janiszt@gmail.com>
2019-06-03 12:13:00 +0200

    Sum all tasks from all frameworks in diag test
    Currenlty we use to have only two frmaeworks Marathon and Metronome so
    we only need to check if we can see their tasks in the bundle. Now we
    have 9 frmaeworks so we need to sum all the tasks we have.
    
    Fixes: DCOS-54487

--- packages/dcos-integration-test/extra/test_dcos_diagnostics.py
+++ packages/dcos-integration-test/extra/test_dcos_diagnostics.py
@@ -732,9 +732,11 @@ def validate_state(zip_state):
     assert isinstance(zip_state, zipfile.ZipExtFile)
     state_output = gzip.decompress(zip_state.read())
     state = json.loads(state_output)
-    assert len(state["frameworks"]) == 2, "bundle must contain information about frameworks"
-    task_count = len(state["frameworks"][1]["tasks"]) + len(state["frameworks"][0]["tasks"])
-    assert task_count == 1, "bundle must contain information about tasks"
+
+    assert len(state["frameworks"]) > 1, "bundle must contain information about frameworks"
+
+    task_count = sum([len(f["tasks"]) for f in state["frameworks"]])
+    assert task_count > 0, "bundle must contains information about tasks"
 
 
 def verify_archived_items(folder, archived_items, expected_files):
