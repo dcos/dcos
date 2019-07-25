ff5d00016bc6b55e6a300a0663b12f692c22bee0
Tim Weidner <tweidner@mesosphere.com>
2019-07-25 16:01:26 +0200

    Changes

--- packages/exhibitor/extra/start_exhibitor.py
+++ packages/exhibitor/extra/start_exhibitor.py
@@ -14,6 +14,7 @@ ZK_TRANSACTIONS = os.path.join(ZK_VAR_DIR, 'transactions')
 TLS_ARTIFACT_LOCATION = '/var/lib/dcos/exhibitor-tls-artifacts'
 CSR_SERVICE_CERT_PATH = '/dcoscertstrap-root-cert.pem'
 PRESHAREDKEY_LOCATION = '/dcoscertstrap.psk'
+EXHIBITOR_TLS_TMP_DIR = '/var/lib/dcos/exhibitor'
 
 
 def get_ca_url(exhibitor_bootstrap_ca_url, bootstrap_url) -> str:
@@ -56,30 +57,39 @@ def gen_tls_artifacts(ca_url, artifacts_path) -> None:
     if psk_path.exists():
         psk = psk_path.read_text()
     else:
-        # Empty PSK results in any CSR being signed by the CA service.
+        # Empty PSK outputs in any CSR being signed by the CA service.
         psk = '""'
 
     server_entity = 'exhibitor-server'
     client_entity = 'exhibitor-client'
 
     print('Initiating {} end entity.'.format(server_entity))
-    result = subprocess.check_output(
-        args=['/opt/mesosphere/bin/dcoscertstrap', 'init-entity', server_entity],
+    output = subprocess.check_output(
+        args=[
+            '/opt/mesosphere/bin/dcoscertstrap',
+            '--output-dir', str(EXHIBITOR_TLS_TMP_DIR),
+            'init-entity', server_entity,
+        ],
         stderr=subprocess.STDOUT,
     )
-    print(result.stdout.decode())
+    print(output.decode())
 
     print('Initiating {} end entity.'.format(client_entity))
-    result = subprocess.check_output(
-        args=['/opt/mesosphere/bin/dcoscertstrap', 'init-entity', client_entity],
+    output = subprocess.check_output(
+        args=[
+            '/opt/mesosphere/bin/dcoscertstrap',
+            '--output-dir', str(EXHIBITOR_TLS_TMP_DIR),
+            'init-entity', client_entity,
+        ],
         stderr=subprocess.STDOUT,
     )
-    print(result.stdout.decode())
+    print(output.decode())
 
     print('Making CSR for {} with IP {}'.format(server_entity, ip))
-    result = subprocess.check_output(
+    output = subprocess.check_output(
         args=[
             '/opt/mesosphere/bin/dcoscertstrap', 'csr', server_entity,
+            '--output-dir', str(EXHIBITOR_TLS_TMP_DIR),
             '--url', ca_url,
             '--ca', str(CSR_SERVICE_CERT_PATH),
             '--psk', psk,
@@ -87,12 +97,13 @@ def gen_tls_artifacts(ca_url, artifacts_path) -> None:
         ],
         stderr=subprocess.STDOUT,
     )
-    print(result.stdout.decode())
+    print(output.decode())
 
     print('Making CSR for {} with IP {}'.format(client_entity, ip))
-    result = subprocess.check_output(
+    output = subprocess.check_output(
         args=[
             '/opt/mesosphere/bin/dcoscertstrap', 'csr', client_entity,
+            '--output-dir', str(EXHIBITOR_TLS_TMP_DIR),
             '--url', ca_url,
             '--ca', str(CSR_SERVICE_CERT_PATH),
             '--psk', psk,
@@ -100,12 +111,13 @@ def gen_tls_artifacts(ca_url, artifacts_path) -> None:
         ],
         stderr=subprocess.STDOUT,
     )
-    print(result.stdout.decode())
+    print(output.decode())
 
     print('Writing TLS artifacts to {}'.format(artifacts_path))
-    result = subprocess.check_output(
+    output = subprocess.check_output(
         args=[
             '/opt/mesosphere/bin/dcoscertstrap', 'create-exhibitor-artifacts',
+            '--output-dir', str(EXHIBITOR_TLS_TMP_DIR),
             '--ca', str(CSR_SERVICE_CERT_PATH),
             '--client-entity', client_entity,
             '--server-entity', server_entity,
@@ -113,7 +125,7 @@ def gen_tls_artifacts(ca_url, artifacts_path) -> None:
         ],
         stderr=subprocess.STDOUT,
     )
-    print(result.stdout.decode())
+    print(output.decode())
 
 
 def get_var_assert_set(name):
@@ -261,7 +273,7 @@ if exhibitor_backend == 'STATIC':
 elif zookeeper_cluster_size == 1:
     print("Exhibitor configured for single master/static backend")
     # A Zookeeper cluster size of 1 is a special case regarding the Exhibitor backend.
-    # It will always result in a static backend independent of the DC/OS configuration.
+    # It will always output in a static backend independent of the DC/OS configuration.
     # This allows for skipping exhibitor.wait() logic that is responsible for waiting
     # for Exhibitor configuration change from standalone mode to ensemble mode.
     # https://jira.mesosphere.com/browse/DCOS-6147
