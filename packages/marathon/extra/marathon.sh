67e69aa98348e71bb3ccaa39cf0846c38c96ebc1
Tim Harper <tharper@mesosphere.com>
2019-08-21 22:41:36 -0600

    Bump Marathon to 1.9.64

--- packages/marathon/extra/marathon.sh
+++ packages/marathon/extra/marathon.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 export LIBPROCESS_IP=$($MESOS_IP_DISCOVERY_COMMAND)
 
 : ${MARATHON_HOSTNAME="$LIBPROCESS_IP"}
-: ${MARATHON_DEFAULT_ACCEPTED_RESOURCE_ROLES="*"}
+: ${MARATHON_DEFAULT_ACCEPTED_RESOURCE_ROLES=}
 : ${MARATHON_MESOS_ROLE="slave_public"}
 : ${MARATHON_MAX_INSTANCES_PER_OFFER=100}
 : ${MARATHON_TASK_LAUNCH_CONFIRM_TIMEOUT=1800000}
@@ -16,6 +16,7 @@ export LIBPROCESS_IP=$($MESOS_IP_DISCOVERY_COMMAND)
 : ${MARATHON_ENABLE_FEATURES="vips,task_killing,external_volumes,gpu_resources"}
 : ${MARATHON_MESOS_AUTHENTICATION_PRINCIPAL="dcos_marathon"}
 : ${MARATHON_MESOS_USER="root"}
+: ${MARATHON_ACCEPTED_RESOURCE_ROLES_DEFAULT_BEHAVIOR="unreserved"}
 
 if [ -z "${MARATHON_DISABLE_ZK_COMPRESSION+x}" ]; then
   MARATHON_ZK_COMPRESSION=""
@@ -42,10 +43,8 @@ export \
   MARATHON_TASK_RESERVATION_TIMEOUT \
   STATSD_UDP_HOST \
   STATSD_UDP_PORT \
-  MARATHON_ZK_COMPRESSION
-
-# TODO (DCOS_OSS-3592) - move this variable to the exported list after MARATHON-8254 is fixed, and remove --default_accepted_resources_roles below
-export -n MARATHON_DEFAULT_ACCEPTED_RESOURCE_ROLES
+  MARATHON_ZK_COMPRESSION \
+  MARATHON_ACCEPTED_RESOURCE_ROLES_DEFAULT_BEHAVIOR
 
 exec $PKG_PATH/marathon/bin/marathon \
     -Duser.dir=/var/lib/dcos/marathon \
@@ -55,5 +54,4 @@ exec $PKG_PATH/marathon/bin/marathon \
     -J-XX:+PrintGCTimeStamps \
     --master zk://zk-1.zk:2181,zk-2.zk:2181,zk-3.zk:2181,zk-4.zk:2181,zk-5.zk:2181/mesos \
     --mesos_leader_ui_url "/mesos" \
-    --metrics_statsd --metrics_statsd_host "$STATSD_UDP_HOST" --metrics_statsd_port "$STATSD_UDP_PORT" \
-    --default_accepted_resource_roles "${MARATHON_DEFAULT_ACCEPTED_RESOURCE_ROLES}"
+    --metrics_statsd --metrics_statsd_host "$STATSD_UDP_HOST" --metrics_statsd_port "$STATSD_UDP_PORT"
