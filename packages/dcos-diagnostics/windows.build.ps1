ab3650937f01fe1050b81befa058999586459918
Dominik Dary <ddary@mesosphere.com>
2019-09-13 17:36:16 +0200

    fixing copy command and typo

--- packages/dcos-diagnostics/windows.build.ps1
+++ packages/dcos-diagnostics/windows.build.ps1
@@ -1,7 +1,11 @@
-$SRC_DIR = "c:\gopath\src\github.com\dcos\dcos-diagnistics\"
+$SRC_DIR = "c:\gopath\src\github.com\dcos\dcos-diagnostics\"
 new-item -itemtype directory "c:\gopath\src\github.com\dcos"
-copy-item -recurse  "c:\pkg\src\dcos-diagnostics" -destination "c:\gopath\src\github.com\dcos\"
+
+copy-item -recurse  -Path c:/pkg/src/dcos-diagnostics/ -Destination c:/gopath/src/github.com/dcos/
+
 Push-Location $SRC_DIR
+
 & make install
+
 new-item -itemtype directory "$env:PKG_PATH/bin"
 Pop-Location
