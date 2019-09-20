823f81ba58da025ca4734dfbf01cea65adbf7713
Dominik Dary <ddary@mesosphere.com>
2019-09-20 14:46:53 +0200

    using LDFLAGS on windows builds

--- packages/dcos-diagnostics/windows.build.ps1
+++ packages/dcos-diagnostics/windows.build.ps1
@@ -5,12 +5,12 @@ copy-item -recurse  -Path c:/pkg/src/dcos-diagnostics/ -Destination c:/gopath/sr
 
 Push-Location $SRC_DIR
 
-$env:VERSION = 0.4.0
-$env:COMMIT = $(git rev-parse --short HEAD)
-$env:LDFLAGS = -X github.com/dcos/dcos-diagnostics/config.Version=$(VERSION) -X github.com/dcos/dcos-diagnostics/config.Commit=$(COMMIT)
-
 $env:GOOS = "windows"
-& go build . -ldflags '$($env:LDFLAGS)'
+$env:VERSION = "0.4.0"
+$env:COMMIT = $(git rev-parse --short HEAD)
+$env:LDFLAGS = "-X github.com/dcos/dcos-diagnostics/config.Version=$env:VERSION -X github.com/dcos/dcos-diagnostics/config.Commit=$env:COMMIT"
+
+go build -ldflags="$env:LDFLAGS" .
 
 new-item -itemtype directory "$env:PKG_PATH/bin"
 Copy-Item -Path "$SRC_DIR/dcos-diagnostics.exe" -Destination "$env:PKG_PATH/bin/dcos-diagnostics.exe"
