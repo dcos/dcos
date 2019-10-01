888ed937f79506c8a2b772ba0e408833cdd2d687
Kostiantyn Oleksiienko <kostiantyn_oleksiienko@epam.com>
2019-10-01 16:01:13 +0300

    Added string to windows.build.ps1 for creating logs folder for openresty

--- packages/adminrouter/windows.build.ps1
+++ packages/adminrouter/windows.build.ps1
@@ -10,4 +10,5 @@ function Unzip {
 }
 Unzip "c:\pkg\src\OpenResty\openresty-1.15.8.2-win64.zip" "c:/tmp/openresty"
 Copy-Item -Recurse -Path "c:\tmp\openresty\openresty-1.15.8.2-win64\*" "$env:PKG_PATH\bin\dcos-adminrouter\"
-Copy-Item -Recurse -Path "C:\pkg\build\extra\src\*" "$env:PKG_PATH\etc\dcos-adminrouter\"
+Copy-Item -Recurse -Path "C:\pkg\build\extra\src\*" "$env:PKG_PATH\etc\dcos-adminrouter\conf"
+New-Item -ItemType Directory "$env:PKG_PATH\etc\dcos-adminrouter\logs"
