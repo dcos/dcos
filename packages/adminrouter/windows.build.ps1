88d8e2f29c4dbcc964b141fe39aeb912e199bb40
Kostiantyn Oleksiienko <kostiantyn_oleksiienko@epam.com>
2019-10-04 15:48:11 +0300

    Changed build.ps1 and buildinfo.json

--- packages/adminrouter/windows.build.ps1
+++ packages/adminrouter/windows.build.ps1
@@ -1,6 +1,7 @@
 $ErrorActionPreference = "stop"
-New-Item -ItemType Directory "$env:PKG_PATH/etc/"
-New-Item  -Path "$env:PKG_PATH/etc/adminrouter/"
+New-Item -ItemType Directory -Path "$env:PKG_PATH/etc/", "$env:PKG_PATH/etc/dcos-adminrouter/"
+New-Item -ItemType Directory -Path "$env:PKG_PATH/etc/dcos-adminrouter/conf/", "$env:PKG_PATH/etc/dcos-adminrouter/logs/"
+New-Item -ItemType Directory -Path "$env:PKG_PATH/bin/", "$env:PKG_PATH/bin/dcos-adminrouter/"
 New-Item -ItemType Directory "c:\tmp"
 
 Add-Type -AssemblyName System.IO.Compression.FileSystem
@@ -8,7 +9,11 @@ function Unzip {
     param([string]$zipfile, [string]$outpath)
     [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
 }
+
 Unzip "c:\pkg\src\OpenResty\openresty-1.15.8.2-win64.zip" "c:/tmp/openresty"
+
+ls "C:\pkg\build\extra\src\"
+
 Copy-Item -Recurse -Path "c:\tmp\openresty\openresty-1.15.8.2-win64\*" "$env:PKG_PATH\bin\dcos-adminrouter\"
-Copy-Item -Recurse -Path "C:\pkg\build\extra\src\*" "$env:PKG_PATH\etc\dcos-adminrouter\conf"
-New-Item -ItemType Directory "$env:PKG_PATH\etc\dcos-adminrouter\logs"
+Copy-Item -Recurse -Path "C:\pkg\build\extra\src\*" "$env:PKG_PATH\etc\dcos-adminrouter\conf\"
+Copy-Item "$env:PKG_PATH\etc\dcos-adminrouter\conf\nginx.agent.conf" "$env:PKG_PATH\etc\dcos-adminrouter\conf\nginx.conf"
