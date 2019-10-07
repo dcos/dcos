f306b61d5e0a7bb21659541b155f5c0fb03a783c
Dominik Dary <ddary@mesosphere.com>
2019-10-07 08:56:36 +0200

    removing dcos-metrics and using azure for releases.

--- build_local_windows.ps1
+++ build_local_windows.ps1
@@ -24,18 +24,28 @@ $myhome = $HOME.replace("\", "/")
 # Write a DC/OS Release tool configuration file which specifies where the build
 # should be published to. This default config makes the release tool push the
 # release to a folder in the current user's home directory.
-if ( ! (Test-Path "dcos-release.config.yaml") ) {
-$config_yaml = 
+$config_yaml =
 "storage: `
-   local: `
-    kind: local_path `
-    path: $myhome/dcos-artifacts `
+    azure: `
+     kind: azure_block_blob `
+     account_name: $env:AZURE_STORAGE_ACCOUNT `
+     account_key: $env:AZURE_STORAGE_ACCESS_KEY `
+     container: dcos `
+     download_url: https://dcos.azureedge.net/dcos/dcos-windows/ `
+    aws: `
+        kind: aws_s3 `
+        access_key_id: $env:AWS_ACCESS_KEY_ID `
+        secret_access_key: $env:AWS_SECRET_ACCESS_KEY `
+        bucket: downloads.dcos.io `
+        object_prefix: dcos/dcos-windows `
+        download_url: https://downloads.dcos.io/dcos/dcos-windows/
 options: `
-  preferred: local `
-  cloudformation_s3_url: https://s3-us-west-2.amazonaws.com/downloads.dcos.io/dcos"
+  preferred: aws `
+  cloudformation_s3_url: https://s3-us-west-2.amazonaws.com/downloads.dcos.io/dcos/dcos-windows"
 
-   $config_yaml | Set-Content -Path "dcos-release.config.yaml" 
-}
+   $config_yaml | Set-Content -Path "dcos-release.config.yaml"
+
+type dcos-release.config.yaml
 
 # Create a python virtual environment to install the DC/OS tools to
 python -m venv "$tmpdir/dcos_build_venv"
@@ -46,4 +56,3 @@ python -m venv "$tmpdir/dcos_build_venv"
 
 # Build a release of DC/OS
 release create $env:USERNAME local_build windows
-
