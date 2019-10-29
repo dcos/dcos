f05824ad0f26f0a6e46699627707c76df0a8ea97
Sergii Matus <sergii_matus@epam.com>
2019-10-29 13:47:00 +0200

    added build_genconf_windows.ps1 script and updated build_local_windows.ps1 to accomodate tar ball generation

--- build_local_windows.ps1
+++ build_local_windows.ps1
@@ -25,7 +25,7 @@ $myhome = $HOME.replace("\", "/")
 # should be published to. This default config makes the release tool push the
 # release to a folder in the current user's home directory.
 if ( ! (Test-Path "dcos-release.config.yaml") ) {
-$config_yaml = 
+$config_yaml =
 "storage: `
    local: `
     kind: local_path `
@@ -34,7 +34,7 @@ options: `
   preferred: local `
   cloudformation_s3_url: https://s3-us-west-2.amazonaws.com/downloads.dcos.io/dcos"
 
-   $config_yaml | Set-Content -Path "dcos-release.config.yaml" 
+   $config_yaml | Set-Content -Path "dcos-release.config.yaml"
 }
 
 # Create a python virtual environment to install the DC/OS tools to
@@ -42,8 +42,18 @@ python -m venv "$tmpdir/dcos_build_venv"
 . "$tmpdir/dcos_build_venv/Scripts/Activate.ps1"
 
 # Install the DC/OS tools
-./prep_local_windows.ps1
-
+#./prep_local_windows.ps1
+./prep_teamcity_windows.ps1
 # Build a release of DC/OS
 release create $env:USERNAME local_build windows
+# Build tar ball for windows. 2 params: packages location and DC/OS variant:
+./build_genconf_windows.ps1 "$HOME\dcos-artifacts\windows"
+## Set AWS Credentials:
+# Set-AWSCredential -ProfileName teamcity-server
+## Upload Tar Ball to dcos.download.io
+# Write-S3Object -BucketName "dcos.download.io" -File "$HOME\dcos-artifacts\windows\dcos_generate_config_win.sh" -CannedACLName public-read
+## Verify that the files were uploaded
+# Get-S3BucketWebsite -BucketName "dcos.download.io"
 
+mkdir -f artifacts
+cp -r -force wheelhouse artifacts/
