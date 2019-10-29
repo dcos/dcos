e9589e053110d9b28ebc93a9bd86529c80289e6d
Sergii Matus <sergii_matus@epam.com>
2019-10-29 13:47:00 +0200

    added build_genconf_windows.ps1 script and updated build_local_windows.ps1 to accomodate tar ball generation

--- build_local_windows.ps1
+++ build_local_windows.ps1
@@ -52,7 +52,15 @@ python -m venv "$tmpdir/dcos_build_venv"
 . "$tmpdir/dcos_build_venv/Scripts/Activate.ps1"
 
 # Install the DC/OS tools
-./prep_local_windows.ps1
-
+#./prep_local_windows.ps1
+./prep_teamcity_windows.ps1
 # Build a release of DC/OS
 release create $env:USERNAME local_build windows
+# Build tar ball for windows. 2 params: packages location and DC/OS variant:
+./build_genconf_windows.ps1 "$HOME\dcos-artifacts\windows"
+## Set AWS Credentials:
+# Set-AWSCredential -ProfileName teamcity-server
+## Upload Tar Ball to dcos.download.io
+# Write-S3Object -BucketName "dcos.download.io" -File "$HOME\dcos-artifacts\windows\dcos_generate_config_win.sh" -CannedACLName public-read
+## Verify that the files were uploaded
+# Get-S3BucketWebsite -BucketName "dcos.download.io"
