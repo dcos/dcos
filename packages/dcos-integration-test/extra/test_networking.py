b1fec3ac9065c7a2aefd015a53a1409f4983948f
Qingchuan Hao <qhao@d2iq.com>
2019-11-06 19:26:11 +0800

    fix review comments

--- packages/dcos-integration-test/extra/test_networking.py
+++ packages/dcos-integration-test/extra/test_networking.py
@@ -1,4 +1,3 @@
-import contextlib
 import enum
 import itertools
 import json
@@ -6,7 +5,6 @@ import logging
 import subprocess
 import time
 import uuid
-from abc import ABC, abstractmethod
 from ipaddress import IPv4Address, IPv4Network
 
 import pytest
@@ -486,7 +484,7 @@ def vip_workload_test(dcos_api_session, container, vip_net, proxy_net, ipv6,
     pm_fmt = '{}/pm-{}'.format(path_id, test_case_id)
     pm_fmt = pm_fmt + '-{{:.{}}}'.format(63 - len(pm_fmt))
     if with_port_mapping_app:
-        pm_container = Container.MESOS if container == Container.POD else container
+        pm_container = marathon.Container.MESOS if container == Container.POD else container
         pm_app = MarathonApp(pm_container, marathon.Network.BRIDGE, proxy_host, host_port=vip_port, app_name_fmt=pm_fmt)
     else:
         pm_app = None
@@ -831,174 +829,20 @@ def test_dcos_net_cluster_identity(dcos_api_session):
     assert cluster_id == cookie, "cluster_id: {}, cookie: {}".format(cluster_id, cookie)
 
 
-class CalicoNetworkTestBase(ABC):
-    CALICO_NETWORK_NAME = 'calico'
-    NETWORK_TYPE = marathon.Network.USER
+@pytest.mark.parametrize('container', [marathon.Container.MESOS])
+def test_calico_container_ip_in_network_cidr(container, dcos_api_session):
+    expanded_config = test_helpers.get_expanded_config()
+    network_cidr = expanded_config["calico_network_cidr"]
 
-    @abstractmethod
-    def get_container_type(self):
-        return None
+    app = MarathonApp(
+        container, marathon.Network.USER, network_name="calico",
+        app_name_fmt='/integration-test/calico-cidr/{}')
+    log.info("application: {}".format(app.app))
 
-    @pytest.fixture(scope="class")
-    def slaves(self, dcos_api_session):
-        return dcos_api_session.slaves + dcos_api_session.public_slaves
-
-    @pytest.fixture(scope="class")
-    def expanded_config(self):
-        expanded_config = test_helpers.get_expanded_config()
-        return expanded_config
-
-    def get_calico_app(self, host):
-        """Returns a calico network application definition with unique id """
-        network_name = self.CALICO_NETWORK_NAME
-        container_type = self.get_container_type()
-        network_type = self.NETWORK_TYPE
-        # NOTE: tasks created by marathon in Enterprise DC/OS will be attached
-        # with a label DCOS_SPACE: task_id. And calico CNI restricts the length
-        # of label value in 63 characters.
-        test_uuid = uuid.uuid4().hex[:5]
-        app_name_fmt = "/integration-test/calico/app-{}-{}-{}".format(
-            str(container_type).lower(), host, test_uuid)
-        marathon_app = MarathonApp(
-            container=container_type,
-            network=network_type,
-            host=host,
-            network_name=network_name,
-            app_name_fmt=app_name_fmt)
-        return marathon_app
-
-    @pytest.fixture(scope="class")
-    def mesos_calico_apps(self, dcos_api_session, slaves):
-        """Returns mesos calico app object in healthy state
-
-        Three applications will be returned if there are more than one slave in
-        the cluster, with two on the same slave, and one on another slave.
-        In case we have only one slave in the cluster, two applications on the
-        same slave will be returned.
-        """
-        only_one_slave = False
-        if len(slaves) == 1:
-            log.warn("Only two applications will be returned for only 1 slave"
-                     " in the cluster")
-            only_one_slave = True
-        marathon_apps = []
-        app_host0_1 = self.get_calico_app(slaves[0])
-        marathon_apps.append(app_host0_1)
-        app_host0_2 = self.get_calico_app(slaves[0])
-        marathon_apps.append(app_host0_2)
-        with contextlib.ExitStack() as stack:
-            stack.enter_context(
-                dcos_api_session.marathon.deploy_and_cleanup(app_host0_1.app))
-            stack.enter_context(
-                dcos_api_session.marathon.deploy_and_cleanup(app_host0_2.app))
-            if not only_one_slave:
-                app_host1 = self.get_calico_app(slaves[1])
-                marathon_apps.append(app_host1)
-                stack.enter_context(
-                    dcos_api_session.marathon.deploy_and_cleanup(
-                        app_host1.app))
-
-            yield marathon_apps
-
-    def test_container_in_overlay_network_cidr(
-            self, dcos_api_session, mesos_calico_apps, expanded_config):
-        network_cidr = expanded_config["calico_network_cidr"]
-        for mesos_calico_app in mesos_calico_apps:
-            contain_ip_address, _ = mesos_calico_app.hostport(dcos_api_session)
-            assert IPv4Address(contain_ip_address) in IPv4Network(network_cidr)
-
-    def test_apps_communication_in_one_node(self, dcos_api_session,
-                                            mesos_calico_apps):
-        server_app = mesos_calico_apps[0]
-        client_app = mesos_calico_apps[1]
-
-        app_host, app_port = server_app.hostport(dcos_api_session)
-        cmd = '/opt/mesosphere/bin/curl -s -f -m 5 ' \
-              'http://{}:{}/test_uuid'.format(app_host, app_port)
-        client_host, client_port = client_app.hostport(dcos_api_session)
-
-        assert ensure_routable(cmd, client_host,
-                               client_port)['test_uuid'] == server_app.uuid
-
-    def test_apps_communication_across_nodes(self, dcos_api_session, slaves,
-                                             mesos_calico_apps):
-        if len(slaves) < 2:
-            pytest.skip(
-                'Not enough slaves for deploying proxy and origin container'
-                ' on different host')
-        server_app = mesos_calico_apps[0]
-        client_app = mesos_calico_apps[2]
-
-        app_host, app_port = server_app.hostport(dcos_api_session)
-        cmd = '/opt/mesosphere/bin/curl -s -f -m 5 ' \
-              'http://{}:{}/test_uuid'.format(app_host, app_port)
-        client_host, client_port = client_app.hostport(dcos_api_session)
-        client_host, client_port = client_app.hostport(dcos_api_session)
-
-        assert ensure_routable(cmd, client_host,
-                               client_port)['test_uuid'] == server_app.uuid
-
-
-class TestCalicoNetworkMesosApp(CalicoNetworkTestBase):
-    def get_container_type(self):
-        return marathon.Container.MESOS
-
-
-class TestCalicoNetworkPod(CalicoNetworkTestBase):
-    def get_container_type(self):
-        return marathon.Container.MESOS
-
-    def get_calico_app(self, host):
-        """Returns a calico network pod definition with unique id """
-        network_name = self.CALICO_NETWORK_NAME
-        container_type = self.get_container_type()
-        network_type = self.NETWORK_TYPE
-        # NOTE: tasks created by marathon in Enterprise DC/OS will be attached
-        # with a label DCOS_SPACE: task_id. And calico CNI restricts the length
-        # of label value in 63 characters.
-        test_uuid = uuid.uuid4().hex[:5]
-        app_name_fmt = "/integration-test/calico/pod-{}-{}-{}".format(
-            str(container_type).lower(), host, test_uuid)
-
-        pod = MarathonPod(
-            network_type,
-            host,
-            pod_name_fmt=app_name_fmt,
-            network_name=network_name)
-        return pod
-
-    @pytest.fixture(scope="class")
-    def mesos_calico_apps(self, dcos_api_session, slaves):
-        """Returns mesos calico applications in healthy state
-
-        Three applications will be returned if there are more than one slave in
-        the cluster, with two on the same slave, and one on another slave.
-        In case we have only one slave in the cluster, two applications on the
-        same slave will be returned.
-        """
-        only_one_slave = False
-        if len(slaves) == 1:
-            log.warn("Only two applications will be returned for only 1 slave"
-                     " in the cluster")
-            only_one_slave = True
-        marathon_pods = []
-        app_host0_1 = self.get_calico_app(slaves[0])
-        marathon_pods.append(app_host0_1)
-        app_host0_2 = self.get_calico_app(slaves[0])
-        marathon_pods.append(app_host0_2)
-        if not only_one_slave:
-            app_host1 = self.get_calico_app(slaves[1])
-            marathon_pods.append(app_host1)
-        with contextlib.ExitStack() as stack:
-            stack.enter_context(
-                dcos_api_session.marathon.deploy_pod_and_cleanup(
-                    app_host0_1.app))
-            stack.enter_context(
-                dcos_api_session.marathon.deploy_pod_and_cleanup(
-                    app_host0_2.app))
-            if not only_one_slave:
-                stack.enter_context(
-                    dcos_api_session.marathon.deploy_pod_and_cleanup(
-                        app_host1.app))
-
-            yield marathon_pods
+    try:
+        app.deploy(dcos_api_session)
+        app.wait(dcos_api_session)
+        contain_ip_address, _ = app.hostport(dcos_api_session)
+        assert IPv4Address(contain_ip_address) in IPv4Network(network_cidr)
+    finally:
+        app.purge(dcos_api_session)
--- test-e2e/test_calico_networking.py
+++ test-e2e/test_calico_networking.py
@@ -6,6 +6,9 @@ different configurations exposed to the operators are enabled.
 """
 import uuid
 from pathlib import Path
+from typing import Iterator
+
+import pytest
 
 from _pytest.fixtures import SubRequest
 from cluster_helpers import wait_for_dcos_oss
@@ -13,6 +16,24 @@ from dcos_e2e.backends import Docker
 from dcos_e2e.cluster import Cluster
 from dcos_e2e.node import Node, Output
 from passlib.hash import sha512_crypt
+
+
+superuser_username = str(uuid.uuid4())
+superuser_password = str(uuid.uuid4())
+
+
+def assert_system_unit_state(node: Node, unit_name: str, active: bool=True) -> None:
+    result = node.run(
+        args=["systemctl show {}".format(unit_name)],
+        output=Output.LOG_AND_CAPTURE,
+        shell=True,
+    )
+    unit_properties = result.stdout.strip().decode()
+    if active:
+        assert "ActiveState=active" in unit_properties
+    else:
+        assert "ActiveState=inactive" in unit_properties
+        assert "ConditionResult=no" in unit_properties
 
 
 def test_calico_disabled(docker_backend: Docker, artifact_path: Path,
@@ -39,34 +60,22 @@ def test_calico_disabled(docker_backend: Docker, artifact_path: Path,
             log_dir=log_dir,
         )
 
-        def _assert_system_unit_inactive(node: Node, unit_name: str) -> None:
-            result = node.run(
-                args=["systemctl show {}".format(unit_name)],
-                output=Output.LOG_AND_CAPTURE,
-                shell=True,
-            )
-            unit_properties = result.stdout.strip().decode()
-            # dcos calico node should be inactive as a result of condition
-            # check failure
-            assert "ActiveState=inactive" in unit_properties
-            assert "ConditionResult=no" in unit_properties
-
         calico_units = ["dcos-calico-felix", "dcos-calico-bird", "dcos-calico-confd"]
         for node in cluster.masters | cluster.agents | cluster.public_agents:
             for unit_name in calico_units:
-                _assert_system_unit_inactive(node, unit_name)
+                assert_system_unit_state(node, unit_name, active=False)
 
 
-def test_calico_vxlan(docker_backend: Docker, artifact_path: Path,
-                      request: SubRequest, log_dir: Path) -> None:
+@pytest.fixture(scope="module")
+def calico_ipip_cluster(docker_backend: Docker, artifact_path: Path,
+                        request: SubRequest, log_dir: Path) -> Iterator[Cluster]:
     with Cluster(
             cluster_backend=docker_backend,
             masters=1,
-            agents=2,
-            public_agents=1,
+            agents=0,
+            public_agents=0,
     ) as cluster:
-        superuser_username = str(uuid.uuid4())
-        superuser_password = str(uuid.uuid4())
+
         config = {
             "superuser_username": superuser_username,
             # We can hash the password with any `passlib`-based method here.
@@ -89,6 +98,10 @@ def test_calico_vxlan(docker_backend: Docker, artifact_path: Path,
             request=request,
             log_dir=log_dir,
         )
+        yield cluster
+
+
+def test_calico_ipip_container_connectivity(calico_ipip_cluster: Cluster) -> None:
 
         environment_variables = {
             "DCOS_LOGIN_UNAME":
@@ -96,23 +109,33 @@ def test_calico_vxlan(docker_backend: Docker, artifact_path: Path,
             "DCOS_LOGIN_PW":
             superuser_password,
             "MASTER_PUBLIC_IP":
-            list(cluster.masters)[0].public_ip_address,
+            list(calico_ipip_cluster.masters)[0].public_ip_address,
             "MASTERS_PRIVATE_IPS":
-            [node.private_ip_address for node in cluster.masters],
+            [node.private_ip_address for node in calico_ipip_cluster.masters],
             "PUBLIC_AGENTS_PRIVATE_IPS":
-            [node.public_ip_address for node in cluster.public_agents],
+            [node.public_ip_address for node in calico_ipip_cluster.public_agents],
             "PRIVATE_AGENTS_PRIVATE_IPS":
-            [node.private_ip_address for node in cluster.agents],
+            [node.private_ip_address for node in calico_ipip_cluster.agents],
         }
+
         pytest_command = [
             "pytest",
             "-vvv",
             "-s",
             "-x",
             "test_calico_networking.py",
+            "-k",
+            "test_calico",
         ]
-        cluster.run_with_test_environment(
+        calico_ipip_cluster.run_with_test_environment(
             args=pytest_command,
             env=environment_variables,
             output=Output.LOG_AND_CAPTURE,
         )
+
+
+def test_calico_ipip_unit_active(calico_ipip_cluster: Cluster) -> None:
+    calico_units = ["dcos-calico-felix", "dcos-calico-bird", "dcos-calico-confd"]
+    for node in calico_ipip_cluster.masters | calico_ipip_cluster.agents | calico_ipip_cluster.public_agents:
+        for unit_name in calico_units:
+            assert_system_unit_state(node, unit_name, active=True)
