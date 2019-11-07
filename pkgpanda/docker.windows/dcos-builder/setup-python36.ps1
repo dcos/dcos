148958a74d09cd4d9150f3c9c6ffbcb4ce15a12d
AleksanderBelov <alwh2004@gmail.com>
2019-11-07 12:38:42 +0200

    fix install Python36

--- pkgpanda/docker.windows/dcos-builder/setup-python36.ps1
+++ pkgpanda/docker.windows/dcos-builder/setup-python36.ps1
@@ -1,7 +1,16 @@
-$pythonDownloadPath = 'c:\temp\python-3.6.8-amd64.exe'
-$pythonInstallDir = Join-Path $env:SystemDrive "Python36"
 $pip_url = 'https://bootstrap.pypa.io/get-pip.py'
+$python_url = 'https://www.python.org/ftp/python/3.6.8/python-3.6.8-amd64.exe'
+$pythonDownloadPath = Join-Path $env:TEMP "python-3.6.8-amd64.exe"
+$pipDownloadPath = Join-Path $env:TEMP "get-pip.py"
+$pythonInstallDir = Join-Path $env:SystemDrive "Python36"
 $INSTALL_ARGS = @("/quiet InstallAllUsers=1 PrependPath=1 Include_test=0 TargetDir=$pythonInstallDir")
+
+Invoke-WebRequest -UseBasicParsing -Uri $python_url -OutFile $pythonDownloadPath
+
+If(!(test-path $pythonInstallDir))
+{
+      New-Item -ItemType Directory -Force -Path $pythonInstallDir
+}
 
 $parameters = @{
 'FilePath' = $pythonDownloadPath
@@ -12,13 +21,10 @@ $parameters = @{
 
 $p = Start-Process @parameters
 if($p.ExitCode -ne 0) {
-Throw "Failed to install $FILENAME_PATH"
+Throw "Failed to install $pythonDownloadPath"
 }
 
 [Environment]::SetEnvironmentVariable("PATH", "${env:path};${pythonInstallDir}\Scripts", "Machine") 
-Invoke-WebRequest -UseBasicParsing -Uri $pip_url -OutFile 'c:\temp\get-pip.py'
+Invoke-WebRequest -UseBasicParsing -Uri $pip_url -OutFile $pipDownloadPath
 
-& $pythonInstallDir\python.exe 'c:\temp\get-pip.py' --no-warn-script-location
-
-
-
+& $pythonInstallDir\python.exe $pipDownloadPath --no-warn-script-location
