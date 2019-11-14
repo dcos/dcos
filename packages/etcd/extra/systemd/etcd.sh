a98666dc02684e50f77202155e8e2d3d1d495b2e
Qingchuan Hao <qhao@d2iq.com>
2019-11-14 10:41:08 -0800

    enable peer-auto-tls

--- packages/etcd/extra/systemd/etcd.sh
+++ packages/etcd/extra/systemd/etcd.sh
@@ -11,9 +11,10 @@ GOMAXPROCS=$(nproc)
 exec /opt/mesosphere/active/etcd/bin/etcd \
      --name "etcd-${IP_PRIVATE}" \
      --data-dir /var/lib/dcos/etcd/default.etcd/ \
-     --listen-peer-urls "http://${IP_PRIVATE}:2380" \
+     --auto-tls --peer-auto-tls \
+     --listen-peer-urls "https://${IP_PRIVATE}:2380" \
      --listen-client-urls "http://${IP_PRIVATE}:2379,http://localhost:2379" \
-     --initial-advertise-peer-urls "http://${IP_PRIVATE}:2380" \
+     --initial-advertise-peer-urls "https://${IP_PRIVATE}:2380" \
      --initial-cluster $CLUSTER_NODES \
      --initial-cluster-state $CLUSTER_STATE \
      --initial-cluster-token "etcd-dcos" \
