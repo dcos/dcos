ef2d426e943869ff0aba27bc7c41fc8b2839db1b
OleksandrBielov <52069511+OleksandrBielov@users.noreply.github.com>
2019-11-28 17:08:07 +0200

    change nssm ver

--- packages/nssm/extra/nssm.ps1
+++ packages/nssm/extra/nssm.ps1
@@ -1,4 +1,4 @@
  param (
     [string]$pkg_inst_dpath
  )
-Copy-Item "$pkg_inst_dpath\bin\nssm.exe" -Destination "c:\Windows\System32" -force
+Copy-Item "$pkg_inst_dpath\bin\nssm.exe" -Destination "c:\Windows" -force
