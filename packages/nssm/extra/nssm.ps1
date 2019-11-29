e53c2fb3d5189d021e12f06abe3f0b54bf88d743
OleksandrBielov <alwh2004@gmail.com>
2019-11-29 10:13:15 +0200

    SymbolicLink to nssm

--- packages/nssm/extra/nssm.ps1
+++ packages/nssm/extra/nssm.ps1
@@ -1,4 +1,4 @@
  param (
     [string]$pkg_inst_dpath
  )
-Copy-Item "$pkg_inst_dpath\bin\nssm.exe" -Destination "c:\Windows" -force
+New-Item -ItemType SymbolicLink -Path "$Env:windir\nssm.exe" -Target "$pkg_inst_dpath\bin\nssm.exe"
