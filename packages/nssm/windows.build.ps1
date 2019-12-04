d0f961e87f3008ccd861a25f8aa0ed0dc774f14b
Jonathan Giddy <jgiddy@mesosphere.com>
2019-12-04 07:10:35 +0000

    Package Jinja2 files in conf directory

--- packages/nssm/windows.build.ps1
+++ packages/nssm/windows.build.ps1
@@ -1,6 +1,6 @@
 $ErrorActionPreference = "stop"
 New-Item -ItemType Directory "$env:PKG_PATH/bin"
-New-Item -ItemType Directory "$env:PKG_PATH/etc"
+New-Item -ItemType Directory "$env:PKG_PATH/conf"
 Add-Type -AssemblyName System.IO.Compression.FileSystem
 function Unzip {
     param([string]$zipfile, [string]$outpath)
@@ -9,5 +9,5 @@ function Unzip {
 Unzip "c:\pkg\src\nssm\nssm-2.24-101-g897c7ad.zip" "c:\pkg\src\nssm\"
 
 Copy-Item -Recurse -Path "c:/pkg/src/nssm/nssm-2.24-101-g897c7ad/win64/*" "$env:PKG_PATH/bin/"
-Copy-Item "pkg/extra/nssm.extra.j2" "$env:PKG_PATH/etc/"
-Copy-Item "pkg/extra/nssm.ps1" "$env:PKG_PATH/etc/"
+Copy-Item "pkg/extra/nssm.extra.j2" "$env:PKG_PATH/conf/"
+Copy-Item "pkg/extra/nssm.ps1" "$env:PKG_PATH/conf/"
