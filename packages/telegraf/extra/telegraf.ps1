29714d004b201e37558370e99c0c35859bfcb2e8
OleksandrBielov <Oleksandr_Bielov1@epam.com>
2019-12-16 14:42:32 +0200

    fix start telegraf service

--- packages/telegraf/extra/telegraf.ps1
+++ packages/telegraf/extra/telegraf.ps1
@@ -1,7 +1,6 @@
  param (
     [string]$pkg_inst_dpath,
-    [string]$dcos_conf_dpath,
-    [string]$dcos_bin_dpath
+    [string]$dcos_conf_dpath
  )
 
 $IPAddress = ([System.Net.Dns]::GetHostByName("master.mesos").AddressList[0]).IpAddressToString
@@ -11,4 +10,25 @@ $local_ip = (Find-NetRoute -RemoteIPAddress $IPAddress | Select-Object  Ipv4Addr
 $env:DCOS_NODE_PRIVATE_IP=$local_ip;
 
 & $pkg_inst_dpath\bin\telegraf.exe --config $dcos_conf_dpath\telegraf\telegraf.conf --service install
+
+function Set-ServiceRecovery{
+    [alias('Set-Recovery')]
+    param
+    (
+        [string] [Parameter(Mandatory=$true)] $ServiceDisplayName,
+        [string] $action1 = "restart",
+        [int] $time1 =  30000, 
+        [string] $action2 = "restart",
+        [int] $time2 =  30000,
+        [string] $actionLast = "restart",
+        [int] $timeLast = 30000, 
+        [int] $resetCounter = 4000 
+    )
+    $services = Get-CimInstance -ClassName 'Win32_Service' | Where-Object {$_.DisplayName -imatch $ServiceDisplayName}
+    $action = $action1+"/"+$time1+"/"+$action2+"/"+$time2+"/"+$actionLast+"/"+$timeLast
+    foreach ($service in $services){
+        $output = sc.exe $serverPath failure $($service.Name) actions= $action reset= $resetCounter
+    }
+}
+Set-ServiceRecovery -ServiceDisplayName "Telegraf Data Collector Service"
 Start-Service -Name telegraf
