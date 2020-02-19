65d41414ce9cf2cdce9ab993404151ee82c235cc
Jonathan Giddy <jgiddy@mesosphere.com>
2020-02-19 10:26:42 +0100

    Add retries to all Windows packages in Docker build

--- pkgpanda/docker.windows/dcos-builder/setup-erlang.ps1
+++ pkgpanda/docker.windows/dcos-builder/setup-erlang.ps1
@@ -1,18 +1,27 @@
-$FILENAME_PATH = "c:\temp\otp_win64_19.3.exe"
+$ErrorActionPreference = "stop"
+
+$url = 'http://erlang.org/download/otp_win64_19.3.exe'
+$file = 'c:\temp\otp_win64_19.3.exe'
+
+Write-Output "Downloading $url"
+
+Invoke-WebRequest -Uri $url -OutFile $file -MaximumRetryCount 5 -RetryIntervalSec 5
+
 $INSTALL_DIR = Join-Path $env:ProgramFiles "erlang"
 $INSTALL_ARGS = @( "/S", "/D=$INSTALL_DIR" )
  
 $parameters = @{
-    'FilePath' = $FILENAME_PATH
+    'FilePath' = $file
     'ArgumentList' = $INSTALL_ARGS
     'Wait' = $true
     'PassThru' = $true
 }
 
-Write-Output "Installing $FILENAME_PATH"
+Write-Output "Installing $file"
 
 $p = Start-Process @parameters
 if($p.ExitCode -ne 0) {
-    Throw "Failed to install prerequisite $FILENAME_PATH during the environment setup"
+    Throw "Failed to install prerequisite $file during the environment setup"
 }
 
+Remove-Item -Path $file
