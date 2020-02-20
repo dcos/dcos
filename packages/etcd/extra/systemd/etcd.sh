9c0dd2920c04f59fe70304ad6b3a499a8fc3d2bd
Tomasz Janiszewski <janiszt@gmail.com>
2020-02-20 14:27:44 +0100

    Move etcd config to env

--- packages/etcd/extra/systemd/etcd.sh
+++ packages/etcd/extra/systemd/etcd.sh
@@ -2,7 +2,7 @@
 
 set -xe
 
-IP_PRIVATE=`/opt/mesosphere/bin/detect_ip`
+IP_PRIVATE=$($MESOS_IP_DISCOVERY_COMMAND)
 CLUSTER_NODES=`cat /run/dcos/etcd/initial-nodes`
 CLUSTER_STATE=`cat /run/dcos/etcd/initial-state`
 
@@ -11,13 +11,11 @@ GOMAXPROCS=$(nproc)
 exec /opt/mesosphere/active/etcd/bin/etcd \
      --name "etcd-${IP_PRIVATE}" \
      --data-dir /var/lib/dcos/etcd/default.etcd/ \
-     --auto-tls --peer-auto-tls \
      --listen-peer-urls "https://${IP_PRIVATE}:2380" \
-     --listen-client-urls "http://${IP_PRIVATE}:2379,http://localhost:2379" \
+     --listen-client-urls "${PROTOCOL}://${IP_PRIVATE}:2379,${PROTOCOL}://localhost:2379" \
      --initial-advertise-peer-urls "https://${IP_PRIVATE}:2380" \
      --initial-cluster $CLUSTER_NODES \
      --initial-cluster-state $CLUSTER_STATE \
      --initial-cluster-token "etcd-dcos" \
-     --advertise-client-urls "http://${IP_PRIVATE}:2379,http://localhost:2379" \
+     --advertise-client-urls "${PROTOCOL}://${IP_PRIVATE}:2379,${PROTOCOL}://localhost:2379" \
      --enable-v2=true
-
