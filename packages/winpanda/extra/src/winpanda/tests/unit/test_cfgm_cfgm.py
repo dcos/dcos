49ca0686666d5d6755f2e3017471bc99fee437c5
Jonathan Giddy <jgiddy@mesosphere.com>
2020-03-06 12:29:48 +0000

    Keep existing configuration files during installation

--- packages/winpanda/extra/src/winpanda/tests/unit/test_cfgm_cfgm.py
+++ packages/winpanda/extra/src/winpanda/tests/unit/test_cfgm_cfgm.py
@@ -5,7 +5,6 @@ import pytest
 from cfgm.cfgm import PkgConfManager
 from cfgm import exceptions
 from core.package.manifest import PackageManifest
-from core.exceptions import InstallationStorageError
 
 
 class TestPkgConfManager(unittest.TestCase):
@@ -22,19 +21,6 @@ class TestPkgConfManager(unittest.TestCase):
         manifest = mock.Mock(spec=PackageManifest)
         manifest.istor_nodes.inst_pkgrepo.joinpath.return_value = path
 
-        return manifest
-
-    @classmethod
-    def get_configured_manifest_mock(cls):
-        path = mock.Mock()
-        path.exists.return_value = True
-        path.is_file.return_value = True
-        path.is_symlink.return_value = False
-        path.is_dir.return_value = False
-        path.unlink.side_effect = RuntimeError()
-
-        manifest = cls.get_manifest_mock(exists=True, is_symlink=False, is_reserved=False, is_dir=True)
-        manifest.istor_nodes.inst_cfg.joinpath.return_value = path
         return manifest
 
     @mock.patch('cfgm.cfgm.isinstance', return_value=True)
@@ -68,10 +54,4 @@ class TestPkgConfManager(unittest.TestCase):
         manifest_mock = self.get_manifest_mock(exists=True, is_symlink=False, is_reserved=False)
         cfg_manager = PkgConfManager(manifest_mock)
         with pytest.raises(exceptions.PkgConfInvalidError):
-            cfg_manager.setup_conf()
-
-    def test_setup_runtime_error_should_fail(self):
-        manifest_mock = self.get_configured_manifest_mock()
-        cfg_manager = PkgConfManager(manifest_mock)
-        with pytest.raises(InstallationStorageError):
             cfg_manager.setup_conf()
