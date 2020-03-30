1b8067c95e050f53365a5098d4cfbfdb83539cc0
Sergii Matus <sergii_matus@epam.com>
2020-03-30 18:47:09 +0300

    fix dcos-diagnostics package to generate agent_service_account.json

---
+++ packages/dcos-diagnostics/extra/dcos-diagnostics.ps1
@@ -0,0 +1,52 @@
+param (
+    [string]$path_to_packages = "c:\d2iq\dcos\packages"
+)
+
+$ErrorActionPreference = "Stop"
+
+function Test-CalledFromPrompt {
+    (Get-PSCallStack)[-2].Command -eq "prompt"
+}
+
+function Invoke-NativeApplication {
+    # The helper function is used to run Native Windows apps in PS1 such as python.exe or cmd.exe. IMPORTANT, it cannot be used to run PS1 CmdLets!
+    # Source code: https://github.com/mnaoumov/Invoke-NativeApplication
+    param
+    (
+        [ScriptBlock] $ScriptBlock,
+        [int[]] $AllowedExitCodes = @(0),
+        [switch] $IgnoreExitCode
+    )
+    [string] $stringScriptBlock = $ScriptBlock.ToString();
+    $backupErrorActionPreference = $ErrorActionPreference;
+    $ErrorActionPreference = "Continue";
+    try {
+        if (Test-CalledFromPrompt) {
+            $lines = & $ScriptBlock
+        }
+        else {
+            $lines = & $ScriptBlock 2>&1
+        }
+        $lines | ForEach-Object -Process `
+            {
+                $isError = $_ -is [System.Management.Automation.ErrorRecord]
+                "$_" | Add-Member -Name IsError -MemberType NoteProperty -Value $isError -PassThru
+            }
+        if ((-not $IgnoreExitCode) -and ($AllowedExitCodes -notcontains $LASTEXITCODE)) {
+            throw "$(Get-Date -Format "yyyy-MM-dd HH:mm:ss") ERROR: Failed to execute `'$stringScriptBlock`' : returned $LASTEXITCODE. Check traceback for more details!" 2>&1
+        }
+    }
+    finally {
+        $ErrorActionPreference = $backupErrorActionPreference
+    }
+}
+
+$packagebootsrapname = [string[]](Get-ChildItem $path_to_packages -Recurse -Filter bootstrap--*)
+$pathtobootstrap = "$path_to_packages\$packagebootsrapname"
+
+$Env:PYTHONPATH = "$pathtobootstrap\lib\python36\site-packages;$pathtobootstrap"
+
+Invoke-NativeApplication {
+    python.exe "$pathtobootstrap\bin\bootstrap" dcos-diagnostics-agent
+}
+
