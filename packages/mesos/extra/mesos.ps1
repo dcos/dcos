b41811385a87a51321607065cf2fb99935015c8f
Jonathan Giddy <jgiddy@mesosphere.com>
2020-04-03 14:40:13 +0100

    Call bootstrap.ps1 when installing Mesos on Windows

--- packages/mesos/extra/mesos.ps1
+++ packages/mesos/extra/mesos.ps1
@@ -1,5 +1,4 @@
 param (
-    [string]$path_to_packages = "c:\d2iq\dcos\packages",
     [string]$path_to_dcos_bin = "c:\d2iq\dcos\bin",
     [string]$path_to_dcos_conf = "c:\d2iq\dcos\etc\mesos"
     )
@@ -7,9 +6,4 @@ param (
 $domain = & $path_to_dcos_bin\fault-domain-detect-win.ps1
 Set-Content $path_to_dcos_conf\domain.json $domain -Force
 
-$packagebootsrapname = [string[]](Get-ChildItem $path_to_packages -Recurse -Filter bootstrap--*)
-$pathtobootstrap = "$path_to_packages\$packagebootsrapname"
-
-$Env:PYTHONPATH = "$pathtobootstrap\lib\python36\site-packages;$pathtobootstrap"
-
-& python "$pathtobootstrap\bin\bootstrap" dcos-mesos-slave
+& $path_to_dcos_bin\bootstrap.ps1 dcos-mesos-slave
