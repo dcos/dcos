b501f3859973ddae9251a61bada02ec0f8bc16f1
Carter Gawron <cgawron@d2iq.com>
2020-04-23 13:27:16 -0700

    Adding default main.tf for Teamcity usage
    main.tf from teamcity parameters used by DCOS Integration Tests

---
+++ teamcity/main.tf
@@ -0,0 +1,51 @@
+provider "aws" {
+  region = "%env.AWS_REGION%"
+}
+
+module "dcos" {
+  source  = "dcos-terraform/dcos/aws"
+  version = "~> 0.2.10"
+
+  cluster_name        = "tf-ci-%teamcity.build.id%-"
+  cluster_name_random_string = true
+
+  ssh_public_key_file = "id_rsa.pub"
+  admin_ips           = ["0.0.0.0/0"]
+
+  num_masters        = "1"
+  num_private_agents = "2"
+  num_public_agents  = "1"
+
+  custom_dcos_download_path = "%ONPREM_INSTALLER_URL%"
+
+  dcos_instance_os    = "centos_7.5"
+  bootstrap_instance_type = "%ONPREM_AWS_INSTANCE_SIZE%"
+  masters_instance_type  = "%ONPREM_AWS_INSTANCE_SIZE%"
+  private_agents_instance_type = "%ONPREM_AWS_INSTANCE_SIZE%"
+  public_agents_instance_type = "%ONPREM_AWS_INSTANCE_SIZE%"
+  #AVAILABILITYZONES
+
+  dcos_calico_network_cidr = "172.17.0.0/16"
+  dcos_variant = "open"
+
+  providers = {
+    aws = "aws"
+  }
+
+  tags = {
+    build_id = "%teamcity.build.id%"
+    build_type_id = "%system.teamcity.buildType.id%"
+  }
+}
+
+output "masters-ips" {
+  value = "${module.dcos.masters-ips}"
+}
+
+output "cluster-address" {
+  value = "${module.dcos.masters-loadbalancer}"
+}
+
+output "public-agents-loadbalancer" {
+  value = "${module.dcos.public-agents-loadbalancer}"
+}
