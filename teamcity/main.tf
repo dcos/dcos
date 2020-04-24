3297386a6ce57c22375da409beb6b6263552f52e
Carter Gawron <cgawron@d2iq.com>
2020-04-24 15:37:43 -0700

    Update main.tf
    Changing variables from Teamcity specific to TF specific

--- teamcity/main.tf
+++ teamcity/main.tf
@@ -1,12 +1,12 @@
 provider "aws" {
-  region = "%env.AWS_REGION%"
+  region = "${var.AWS_REGION}"
 }
 
 module "dcos" {
   source  = "dcos-terraform/dcos/aws"
   version = "~> 0.2.10"
 
-  cluster_name        = "tf-ci-%teamcity.build.id%-"
+  cluster_name        = "tf-ci-${var.teamcity.build.id}-"
   cluster_name_random_string = true
 
   ssh_public_key_file = "id_rsa.pub"
@@ -16,13 +16,13 @@ module "dcos" {
   num_private_agents = "2"
   num_public_agents  = "1"
 
-  custom_dcos_download_path = "%ONPREM_INSTALLER_URL%"
+  custom_dcos_download_path = "${var.ONPREM_INSTALLER_URL}"
 
   dcos_instance_os    = "centos_7.5"
-  bootstrap_instance_type = "%ONPREM_AWS_INSTANCE_SIZE%"
-  masters_instance_type  = "%ONPREM_AWS_INSTANCE_SIZE%"
-  private_agents_instance_type = "%ONPREM_AWS_INSTANCE_SIZE%"
-  public_agents_instance_type = "%ONPREM_AWS_INSTANCE_SIZE%"
+  bootstrap_instance_type = "${var.ONPREM_AWS_INSTANCE_SIZE}"
+  masters_instance_type  = "${var.ONPREM_AWS_INSTANCE_SIZE}"
+  private_agents_instance_type = "${var.ONPREM_AWS_INSTANCE_SIZE}"
+  public_agents_instance_type = "${var.ONPREM_AWS_INSTANCE_SIZE}"
   #AVAILABILITYZONES
 
   dcos_calico_network_cidr = "172.17.0.0/16"
@@ -33,8 +33,8 @@ module "dcos" {
   }
 
   tags = {
-    build_id = "%teamcity.build.id%"
-    build_type_id = "%system.teamcity.buildType.id%"
+    build_id = "${var.teamcity.build.id}"
+    build_type_id = "${var.system.teamcity.buildType.id}"
   }
 }
 
