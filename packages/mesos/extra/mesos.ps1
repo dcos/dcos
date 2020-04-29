acccf044af434f073651af4c334e5594461f8dab
Greg Mann <gregorywmann@gmail.com>
2020-04-29 16:30:35 -0700

    WIP: revert Mesos to pre-monolith build.

--- packages/mesos/extra/mesos.ps1
+++
@@ -1,9 +0,0 @@
-param (
-    [string]$path_to_dcos_bin = "c:\d2iq\dcos\bin",
-    [string]$path_to_dcos_conf = "c:\d2iq\dcos\etc\mesos"
-    )
-
-$domain = & $path_to_dcos_bin\fault-domain-detect-win.ps1
-Set-Content $path_to_dcos_conf\domain.json $domain -Force
-
-& $path_to_dcos_bin\bootstrap.ps1 dcos-mesos-slave
